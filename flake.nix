333643c7327dfba3cb834577ef7514ec2a926e48
Xavier Ruiz <github@xav.ie>
2023-11-01 23:05:55 -0400

    feat(inputs.nixpkgs-bleeding): add

--- flake.nix
+++ flake.nix
@@ -9,7 +9,12 @@
     ];
   };
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    nixpkgs-bleeding = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
