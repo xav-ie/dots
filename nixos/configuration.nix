308228af3e73a8507f49e947b486204370903721
xav-ie <xruizify@gmail.com>
2023-11-14 09:48:00 -0500

    test_3

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
+# test_3
 { inputs, lib, config, pkgs, fetchFromGithub, ... }:
 {
   imports = [ 
@@ -31,7 +32,6 @@
     '';
 
   };
-
   networking = {
     hostName = "nixos"; # Define your hostname.
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
