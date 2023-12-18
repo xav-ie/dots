a668632b91e09c7926abc853f41acad619e54955
Xavier Ruiz <github@xav.ie>
2023-12-18 08:39:01 -0500

    refactor

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -2,8 +2,9 @@
   # darwin prefs and config items
   programs.zsh.enable = true;
   environment = {
-    shells = [pkgs.bash pkgs.zsh];
     loginShell = pkgs.zsh;
+    shells = [pkgs.bash pkgs.zsh];
+    systemPackages = [pkgs.coreutils];
     pathsToLink = ["/Applications"];
   };
   nix.extraOptions = ''
@@ -16,20 +17,19 @@
   # https://github.com/nix-community/home-manager/issues/4026
   users.users.xavierruiz.home = "/Users/xavierruiz";
   system = {
-    #packages = [pkgs.coreutils];
-    keyboard.enableKeyMapping = true;
-    keyboard.remapCapsLockToEscape = true;
     defaults = {
+      dock = {
+        autohide = true;
+      };
       finder = {
         AppleShowAllExtensions = true;
         _FXShowPosixPathInTitle = true;
       };
-      dock = {
-        autohide = true;
-      };
       NSGlobalDomain.InitialKeyRepeat = 14;
       NSGlobalDomain.KeyRepeat = 1;
     };
+    keyboard.enableKeyMapping = true;
+    keyboard.remapCapsLockToEscape = true;
   };
   homebrew = {
     enable = true;
