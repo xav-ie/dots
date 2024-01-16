410929ad32ef27cf15234efcab3320f204ef3d33
Xavier Ruiz <github@xav.ie>
2024-01-15 22:28:31 -0500

    refactor(overlays): move overlays to central location

--- flake.nix
+++ flake.nix
@@ -60,6 +60,34 @@
         (self: super: {
           ctpv = inputs.ctpv.packages.${self.system}.default;
         })
+        (self: super: {
+          mpv = super.mpv.override {
+            scripts = with self.mpvScripts; [
+              autoload # autoloads entries before and after current entry
+              # TODO: only add if on linux
+              # mpris # extends mpv to be controllable with MPD
+              mpv-playlistmanager # resolves url titles, SHIFT+ENTER for playlist
+              quality-menu # control video quality on the fly
+              webtorrent-mpv-hook # extends mpv to handle magnet URLs
+            ];
+          };
+          # use full ffmpeg version to support all video formats
+          # mpv-unwrapped = super.mpv-unwrapped.override {
+          # ffmpeg_5 = ffmpeg_5-full;
+          # };
+          weechat = super.weechat.override {
+            configure = { availablePlugins, ... }: {
+              scripts = with super.weechatScripts; [
+                # Idk how to use this one yet
+                edit # edit messages in $EDITOR
+                wee-slack # slack in weechat
+                # I think weeslack already has way to facilitate notifications
+                # weechat-notify-send # highlight and notify bindings to notify-send
+                weechat-go # command pallette jumping
+              ];
+            };
+          };
+        })
       ];
     in
     {
