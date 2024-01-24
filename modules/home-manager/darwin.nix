80e597866dadbe2b5077ef4b16c4114e0bf2d59b
Xavier Ruiz <github@xav.ie>
2024-01-24 17:31:26 -0500

    chore(cleanup): general cleanup and refactoring

--- modules/home-manager/darwin.nix
+++ modules/home-manager/darwin.nix
@@ -1,33 +1,15 @@
-{
-  pkgs,
-  ...
-} @ inputs: {
+{ ... }: {
   home = {
-    packages = [pkgs.yabai pkgs.wezterm];
+    packages = [ ];
     # The state version is required and should stay at the version you
     # originally installed.
     stateVersion = "23.11";
-    sessionVariables = {
-    };
+    sessionVariables = { };
   };
   programs = {
-    alacritty = {
-      enable = true;
-      settings = {
-        font.normal.family = "MesloLGS Nerd Font Mono";
-        font.size = 14;
-        window = {
-          decorations = "Transparent"; # "Transparent" is mac only
-          opacity = 0.9;
-          blur = true; # does not seem to work...
-          #option_as_alt = "Both"; # mac only
-        };
-      };
-    };
     zsh = {
       initExtra = '' '';
     };
   };
   home.file.".config/borders/bordersrc".source = ./dotfiles/bordersrc;
-  home.file.".config/wezterm/wezterm.lua".source = ./dotfiles/wezterm.lua;
 }
