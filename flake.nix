4a46b2d71f665851867d33eebff44988a4b2b2b9
Xavier Ruiz <github@xav.ie>
2024-01-30 17:49:50 -0500

    update(flakes)

--- flake.nix
+++ flake.nix
@@ -21,6 +21,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hyprland-contrib = {
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
