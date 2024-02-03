9230e54a06f12c07739017a9d86b08d8ede1bbbb
Xavier Ruiz <github@xav.ie>
2024-02-03 09:13:00 -0500

    cleanup

--- flake.nix
+++ flake.nix
@@ -32,8 +32,8 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
-    nixpkgs-bleeding = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable = {
+      url = "github:nixos/nixpkgs/nixos-23.11";
     };
     nur = {
       url = "github:nix-community/NUR";
@@ -58,28 +58,25 @@
     , ...
     } @ inputs:
     let
-      # TODO: what does setting this do?
-      # nix.registry.nixpkgs.flake = nixpkgs;
-      # TODO: move to overlays because they are supposed to be better but I can't seem to figure them out :(
-      # Some other ppl who got them working:
+      # Good nix configs:
       # https://github.com/clemak27/linux_setup/blob/4970745992be98b0d00fdae336b4b9ee63f3c1af/flake.nix#L48
       # https://github.com/CosmicHalo/AndromedaNixos/blob/665668415fa72e850d322adbdacb81c1251301c0/overlays/zjstatus/default.nix#L2
       overlays = [
         nur.overlay
         (self: super: {
           ctpv = inputs.ctpv.packages.${self.system}.default;
-        })
-        (self: super: {
           mpv = super.mpv.override {
             scripts = with self.mpvScripts; [
               autoload # autoloads entries before and after current entry
-              # TODO: only add if on linux
-              # mpris # extends mpv to be controllable with MPD
               mpv-playlistmanager # resolves url titles, SHIFT+ENTER for playlist
               quality-menu # control video quality on the fly
               webtorrent-mpv-hook # extends mpv to handle magnet URLs
-            ];
+            ] ++
+            # extends mpv to be controllable with MPD
+            self.lib.optional (self.system == "x86_64-linux") self.mpvScripts.mpris
+            ;
           };
+          # TODO: do I need this?
           # use full ffmpeg version to support all video formats
           # mpv-unwrapped = super.mpv-unwrapped.override {
           # ffmpeg_5 = ffmpeg_5-full;
@@ -98,17 +95,24 @@
           };
         })
       ];
+      nixModule = ({ config, pkgs, ... }: {
+        nixpkgs.overlays = [ overlays ];
+        # This setting is important because it makes things like:
+        # `nix run nixpkgs#some-package` makes it use the same reference of packages as in your 
+        # flake.lock, which helps prevent the package from being different every time you run it
+        nix.registry.nixpkgs.flake = self.inputs.nixpkgs;
+        nixpkgs.config = {
+          allowUnfree = true;
+        };
+      });
     in
     {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs nur wezterm zjstatus; };
           modules = [
+            nixModule
             ./nixos/configuration.nix
-            # I have no idea if this is working... :/
-            ({ pkgs, ... }: {
-              nixpkgs.overlays = overlays;
-            })
             nur.nixosModules.nur
             home-manager.nixosModules.home-manager
             {
@@ -121,35 +125,33 @@
                   ./modules/home-manager/linux.nix
                 ];
               };
-              # does this have any effect?
-              nixpkgs.overlays = overlays;
             }
           ];
         };
       };
-      darwinConfigurations = {
-        Xaviers-MacBook-Air = darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          pkgs = import inputs.nixpkgs { system = "aarch64-darwin"; overlays = overlays; };
-          specialArgs = { inherit inputs nur zjstatus; };
-          modules = [
-            ./modules/darwin
-            home-manager.darwinModules.home-manager
-            {
-              home-manager = {
-                extraSpecialArgs = { inherit inputs nur zjstatus; };
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.xavierruiz.imports = [
-                  ./modules/home-manager/default.nix
-                  ./modules/home-manager/darwin.nix
-                ];
-              };
-              # does this have any effect?
-              nixpkgs.overlays = overlays;
-            }
-          ];
+      darwinConfigurations = let system = "aarch64-darwin"; in
+        {
+          Xaviers-MacBook-Air = darwin.lib.darwinSystem {
+            inherit system;
+            pkgs = import inputs.nixpkgs { inherit system overlays; };
+            specialArgs = { };
+            modules = [
+              nixModule
+              ./modules/darwin
+              home-manager.darwinModules.home-manager
+              {
+                home-manager = {
+                  extraSpecialArgs = { inherit inputs nur zjstatus; };
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  users.xavierruiz.imports = [
+                    ./modules/home-manager/default.nix
+                    ./modules/home-manager/darwin.nix
+                  ];
+                };
+              }
+            ];
+          };
         };
-      };
     };
 }
