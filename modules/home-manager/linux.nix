6478e604adf6e7246483a1ba05a54aacb430894b
Xavier Ruiz <github@xav.ie>
2024-03-05 19:31:26 -0500

    config(firefox userChrome): make side bar better

--- modules/home-manager/linux.nix
+++ modules/home-manager/linux.nix
@@ -127,16 +127,48 @@ in
           (import ./programs/firefox/settings.nix)
         ];
         userChrome = ''
-          /* hides the native tabs */
+          /* ########  Sidetabs Styles  ######### */
+
+          /* ~~~~~~~~ Autohiding styles ~~~~~~~~~ */
+          :root {
+           --sidebar-hover-width: 36px;
+           --sidebar-visible-width: 190px;
+           --sidebar-debounce-delay: 50ms;
+ 
+          }
+          #sidebar-box {
+           display: grid !important;
+           min-width: var(--sidebar-hover-width) !important;
+           max-width: var(--sidebar-hover-width) !important;
+           overflow: visible !important;
+           height: 100% !important;
+           min-height: 100% !important;
+           max-height: 100% !important;
+          }
+          #sidebar {
+           height: 100% !important;
+           width: var(--sidebar-hover-width) !important;
+           z-index: 200 !important;
+           position: absolute !important;
+           transition: width 150ms var(--sidebar-debounce-delay) ease !important;
+           min-width: 0 !important;
+          }
+          #sidebar:hover {
+           width: var(--sidebar-visible-width) !important;
+          }
+          /* ~~~~~~~~ Hidden elements styles ~~~~~~~~~ */
           #TabsToolbar {
-            visibility: collapse;
+          	display: none !important;
           }
           #titlebar {
-            visibility: collapse;
+          	display: none !important;
           }
           #sidebar-header {
-            visibility: collapse !important;
+          	display: none !important;
           }
+          /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+          /* #################################### */
         '';
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           bitwarden
