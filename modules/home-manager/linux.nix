080f22eda12a8d6adf6c9bb19ce6632a19103741
Xavier Ruiz <github@xav.ie>
2024-03-30 00:54:57 -0400

    fix(Environment): I guess systemd user services need explicit environment for binaries defined

--- modules/home-manager/linux.nix
+++ modules/home-manager/linux.nix
@@ -196,58 +196,59 @@ in
     };
   };
 
-  systemd = {
-    user = {
-      services.ollama-server = {
-        Unit = {
-          Description = "Run ollama server";
-        };
-        Install = {
-          WantedBy = [ "default.target" ];
-        };
-        Service = {
-          ExecStart = "${pkgs.ollama}/bin/ollama serve";
-        };
+  systemd.user = {
+    services.ollama-server = {
+      Unit = {
+        Description = "Run ollama server";
       };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.ollama}/bin/ollama serve";
+      };
+    };
 
-      # Zoom and Slack love to "helpfully" stay open even when I kill them through UI
-      services.kill-spyware = {
-        Unit = {
-          Description = "Stop spyware";
-        };
-        Service = {
-          Type = "oneshot";
-          ExecStart = ./dotfiles/kill-spyware.sh;
-        };
+    # Zoom and Slack love to "helpfully" stay open even when I kill them through UI
+    services.kill-spyware = {
+      Unit = {
+        Description = "Stop spyware";
       };
-      timers.kill-spyware = {
-        Unit = { Description = "Stop spyware after working hours"; };
-        Timer = {
-          Unit = "kill-spyware.service";
-          OnCalendar = "18:00";
-          Persistent = true;
-        };
-        Install = { WantedBy = [ "timers.target" ]; };
+      Service = {
+        Type = "oneshot";
+        ExecStart = ./dotfiles/kill-spyware.sh;
+        # I guess you need this? :/
+        Environment = "PATH=/run/current-system/sw/bin";
       };
+    };
+    timers.kill-spyware = {
+      Unit = { Description = "Stop spyware after working hours"; };
+      Timer = {
+        Unit = "kill-spyware.service";
+        OnCalendar = "18:00";
+        Persistent = true;
+      };
+      Install = { WantedBy = [ "timers.target" ]; };
+    };
 
-      services.start-work = {
-        Unit = {
-          Description = "Start work programs";
-        };
-        Service = {
-          Type = "oneshot";
-          ExecStart = ./dotfiles/start-work.sh;
-        };
+    services.start-work = {
+      Unit = {
+        Description = "Start work programs";
       };
-      timers.start-work = {
-        Unit = { Description = "Start work programs every weekday at 9am"; };
-        Timer = {
-          Unit = "start-work.service";
-          OnCalendar = "Mon..Fri 09:00";
-          Persistent = true;
-        };
-        Install = { WantedBy = [ "timers.target" ]; };
+      Service = {
+        Type = "oneshot";
+        ExecStart = ./dotfiles/start-work.sh;
+        Environment = "PATH=/run/current-system/sw/bin";
       };
+    };
+    timers.start-work = {
+      Unit = { Description = "Start work programs every weekday at 9am"; };
+      Timer = {
+        Unit = "start-work.service";
+        OnCalendar = "Mon..Fri 09:00";
+        Persistent = true;
+      };
+      Install = { WantedBy = [ "timers.target" ]; };
     };
   };
 
