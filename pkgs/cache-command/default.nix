ca9955f29923f45cf75a68f48b744bf732de9a88
Xavier Ruiz <github@xav.ie>
2024-04-02 07:34:40 -0400

    feat(custom packages): add custom packages dir which is included through overlays

---
+++ pkgs/cache-command/default.nix
@@ -0,0 +1,31 @@
+{ writeShellApplication, }:
+writeShellApplication {
+  name = "cache-command";
+  # runtimeInputs = [ jq slurp ];
+  text = ''
+    # Create a hash of the entire command line to use as a cache file name
+    cmd_hash=$(echo "$*" | md5sum | cut -d' ' -f1)
+    cache_dir="$HOME/.cache/cmd_cache"
+    cache_file="''${cache_dir}/''${cmd_hash}"
+
+    mkdir -p "''${cache_dir}"
+
+    # Determine if the cache file exists and is less than an hour old
+    update_cache=1
+    if [ -f "''${cache_file}" ]; then
+        current_time=$(date +%s)
+        last_modified=$(date -r "''${cache_file}" +%s)
+        if [ $((current_time - last_modified)) -lt 3600 ]; then
+            update_cache=0
+        fi
+    fi
+
+    # Execute the command and cache its output if necessary
+    if [ "''${update_cache}" -eq 1 ]; then
+        "$@" > "''${cache_file}"
+    fi
+
+    # Display the cached output
+    cat "''${cache_file}"
+  '';
+}
