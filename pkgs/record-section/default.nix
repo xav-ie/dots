97e12c739fcf396ef6565a23490ad9763ada93b9
xav-ie <github@xav.ie>
2024-04-10 11:01:38 -0400

    fix(record and record-section): properly quote $@

--- pkgs/record-section/default.nix
+++ pkgs/record-section/default.nix
@@ -3,6 +3,6 @@ writeShellApplication {
   name = "record-section";
   runtimeInputs = [ slurp wf-recorder ];
   text = ''
-    sleep 2 && wf-recorder --  -r 60 -c libsvtav1 -C aac -f recording.mp4 -g "$(slurp)" $@
+    sleep 2 && wf-recorder --  -r 60 -c libsvtav1 -C aac -f recording.mp4 -g "$(slurp)" "$@"
   '';
 }
