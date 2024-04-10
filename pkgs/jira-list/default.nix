0a451ffa9ff0ea0a043feb5d9fdd9c36f9637d87
Xavier Ruiz <github@xav.ie>
2024-04-10 08:59:28 -0400

    TODO: add pkg todos

--- pkgs/jira-list/default.nix
+++ pkgs/jira-list/default.nix
@@ -2,6 +2,7 @@
 writeShellApplication {
   name = "jira-list";
   runtimeInputs = [ go-jira fzf cache-command ];
+  # TODO: add action on select, add open-in-browser
   text = ''
     cache-command jira ls | sort | fzf --preview="echo {} | head -c 7 | xargs cache-command jira view" --tac
   '';
