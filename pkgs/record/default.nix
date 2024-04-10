345553eb0fde9b4cf5ccab0a51d0c4c56086af89
Xavier Ruiz <github@xav.ie>
2024-04-10 08:31:10 -0400

    fix(record and record-section): properly pass args

--- pkgs/record/default.nix
+++ pkgs/record/default.nix
@@ -4,6 +4,6 @@ writeShellApplication {
   runtimeInputs = [ wf-recorder ];
   text = ''
     # I am not sure if I should always record with audio or not
-    sleep 2 && wf-recorder --  -r 60 -c libsvtav1 -C aac -f recording.mp4 "$@"
+    sleep 2 && wf-recorder -r 60 -c libsvtav1 -C aac -f recording.mp4 "$@"
   '';
 }
