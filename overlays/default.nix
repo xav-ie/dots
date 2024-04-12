1214b5df3be35e7640591783c2f9b81e608aaf20
xav-ie <github@xav.ie>
2024-04-12 10:44:48 -0400

    refactor(overlays): more manageable configuration

--- overlays/default.nix
+++ overlays/default.nix
@@ -25,4 +25,38 @@
   #   vimPlugins = (prev.vimPlugins or { }) // import ../pkgs/vim-plugins { pkgs = final; };
   # };
 
+  modifications = final: prev: {
+    ctpv = inputs.ctpv.packages.${final.system}.default;
+    alacritty-theme = inputs.alacritty-theme.packages.${final.system};
+    ollama = inputs.ollama.packages.${final.system}.default;
+    mpv = prev.mpv.override {
+      scripts = with final.mpvScripts; [
+        autoload # autoloads entries before and after current entry
+        mpv-playlistmanager # resolves url titles, SHIFT+ENTER for playlist
+        quality-menu # control video quality on the fly
+        webtorrent-mpv-hook # extends mpv to handle magnet URLs
+      ] ++
+      # extends mpv to be controllable with MPD
+      final.lib.optional (final.system == "x86_64-linux") final.mpvScripts.mpris
+      ;
+    };
+    # TODO: do I need this?
+    # use full ffmpeg version to support all video formats
+    # mpv-unwrapped = prev.mpv-unwrapped.override {
+    # ffmpeg_5 = ffmpeg_5-full;
+    # };
+    weechat = prev.weechat.override {
+      configure = { availablePlugins, ... }: {
+        scripts = with prev.weechatScripts; [
+          # Idk how to use this one yet
+          edit # edit messages in $EDITOR
+          wee-slack # slack in weechat
+          # I think weeslack already has way to facilitate notifications
+          # weechat-notify-send # highlight and notify bindings to notify-send
+          weechat-go # command pallette jumping
+        ];
+      };
+    };
+
+  };
 }
