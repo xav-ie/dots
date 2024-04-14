a6431d74fbe2e3375dde5e649019b3821d630c10
Xavier Ruiz <github@xav.ie>
2024-04-14 17:12:44 -0400

    config(zjstatus): fix uair status

--- modules/home-manager/programs/zellij/default.nix
+++ modules/home-manager/programs/zellij/default.nix
@@ -26,10 +26,11 @@ let
               mode_tmux               "#[bg=red] {name} "
               tab_normal              "#[fg=#6C7086] {name} "
               tab_active              "#[fg=magenta,bold,italic] {name} "
+
               datetime                "#[fg=cyan,bold] {format} "
-              datetime_format         "%A, %d %b %I:%M"
+              datetime_format         "%m/%d %I:%M"
               datetime_timezone       "America/New_York"
-              command_pomo_command    "uairctl listen"
+              command_pomo_command    "uairctl fetch '{time}'"
               command_pomo_format     "#[fg=blue] {stdout}"
               command_pomo_interval   "1"
               command_pomo_rendermode "static"
