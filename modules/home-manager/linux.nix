99eb4d86fc4633fa3f0a53188717538e41a4cc7f
Xavier Ruiz <github@xav.ie>
2024-04-17 17:45:22 -0400

    format

--- modules/home-manager/linux.nix
+++ modules/home-manager/linux.nix
@@ -1,12 +1,6 @@
-{ pkgs
-, lib
-, inputs
-, ...
-}:
-let
-  merge = lib.foldr (a: b: a // b) { };
-in
-{
+{ pkgs, lib, inputs, ... }:
+let merge = lib.foldr (a: b: a // b) { };
+in {
   home = {
     packages = (with pkgs; [
       ################################
@@ -100,10 +94,7 @@ in
         hyprlandSupport = true;
       }))
 
-    ]) ++ [
-      inputs.hyprland-contrib.packages."x86_64-linux".grimblast
-    ]
-    ;
+    ]) ++ [ inputs.hyprland-contrib.packages."x86_64-linux".grimblast ];
     # The state version is required and should stay at the version you
     # originally installed.
     stateVersion = "23.11";
@@ -127,50 +118,51 @@ in
           (import ./programs/firefox/annoyances.nix)
           (import ./programs/firefox/settings.nix)
         ];
-        userChrome = /*css*/''
-          /* ########  Sidetabs Styles  ######### */
+        userChrome = # css
+          ''
+            /* ########  Sidetabs Styles  ######### */
 
-          /* ~~~~~~~~ Autohiding styles ~~~~~~~~~ */
-          :root {
-           --sidebar-hover-width: 36px;
-           --sidebar-visible-width: 190px;
-           --sidebar-debounce-delay: 50ms;
- 
-          }
-          #sidebar-box {
-           display: grid !important;
-           min-width: var(--sidebar-hover-width) !important;
-           max-width: var(--sidebar-hover-width) !important;
-           overflow: visible !important;
-           height: 100% !important;
-           min-height: 100% !important;
-           max-height: 100% !important;
-          }
-          #sidebar {
-           height: 100% !important;
-           width: var(--sidebar-hover-width) !important;
-           z-index: 200 !important;
-           position: absolute !important;
-           transition: width 150ms var(--sidebar-debounce-delay) ease !important;
-           min-width: 0 !important;
-          }
-          #sidebar:hover {
-           width: var(--sidebar-visible-width) !important;
-          }
-          /* ~~~~~~~~ Hidden elements styles ~~~~~~~~~ */
-          #TabsToolbar {
-          	display: none !important;
-          }
-          #titlebar {
-          	display: none !important;
-          }
-          #sidebar-header {
-          	display: none !important;
-          }
-          /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+            /* ~~~~~~~~ Autohiding styles ~~~~~~~~~ */
+            :root {
+             --sidebar-hover-width: 36px;
+             --sidebar-visible-width: 190px;
+             --sidebar-debounce-delay: 50ms;
 
-          /* #################################### */
-        '';
+            }
+            #sidebar-box {
+             display: grid !important;
+             min-width: var(--sidebar-hover-width) !important;
+             max-width: var(--sidebar-hover-width) !important;
+             overflow: visible !important;
+             height: 100% !important;
+             min-height: 100% !important;
+             max-height: 100% !important;
+            }
+            #sidebar {
+             height: 100% !important;
+             width: var(--sidebar-hover-width) !important;
+             z-index: 200 !important;
+             position: absolute !important;
+             transition: width 150ms var(--sidebar-debounce-delay) ease !important;
+             min-width: 0 !important;
+            }
+            #sidebar:hover {
+             width: var(--sidebar-visible-width) !important;
+            }
+            /* ~~~~~~~~ Hidden elements styles ~~~~~~~~~ */
+            #TabsToolbar {
+            	display: none !important;
+            }
+            #titlebar {
+            	display: none !important;
+            }
+            #sidebar-header {
+            	display: none !important;
+            }
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            /* #################################### */
+          '';
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           bitwarden
           ublock-origin
@@ -199,22 +191,14 @@ in
 
   systemd.user = {
     services.ollama-server = {
-      Unit = {
-        Description = "Run ollama server";
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-      Service = {
-        ExecStart = "${pkgs.ollama}/bin/ollama serve";
-      };
+      Unit = { Description = "Run ollama server"; };
+      Install = { WantedBy = [ "default.target" ]; };
+      Service = { ExecStart = "${pkgs.ollama}/bin/ollama serve"; };
     };
 
     # Zoom and Slack love to "helpfully" stay open even when I kill them through UI
     services.kill-spyware = {
-      Unit = {
-        Description = "Stop spyware";
-      };
+      Unit = { Description = "Stop spyware"; };
       Service = {
         Type = "oneshot";
         ExecStart = ./dotfiles/kill-spyware.sh;
@@ -233,9 +217,7 @@ in
     };
 
     services.start-work = {
-      Unit = {
-        Description = "Start work programs";
-      };
+      Unit = { Description = "Start work programs"; };
       Service = {
         Type = "oneshot";
         ExecStart = ./dotfiles/start-work.sh;
