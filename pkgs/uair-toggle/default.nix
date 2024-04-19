0db8a5b26f9c2665d062d61069dc10c4f8a3987c
Xavier Ruiz <github@xav.ie>
2024-04-19 17:43:19 -0400

    feat(pkgs): uair-toggle -> uair-toggle-and-notify I want a special notification for when a new session starts

--- pkgs/uair-toggle/default.nix
+++
@@ -1,8 +0,0 @@
-{ writeShellApplication, uair }:
-writeShellApplication {
-  name = "uair-toggle";
-  runtimeInputs = [ uair ];
-  text = ''
-    uairctl toggle || { nohup uair > /dev/null 2>&1 & disown; sleep 2 && uairctl toggle; }
-  '';
-}
