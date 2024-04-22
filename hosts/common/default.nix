dc47a7933ee858a1da6246c35d074d6704523d5d
Xavier Ruiz <github@xav.ie>
2024-04-22 00:32:29 -0400

    format

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,17 +1,14 @@
+{ pkgs, inputs, ... }:
 {
-  config,
-  pkgs,
-  inputs,
-  ...
-}:
-{
-  nix.registry = {
-    # This setting is important because it makes things like:
-    # `nix run nixpkgs#some-package` makes it use the same reference of packages as in your 
-    # flake.lock, which helps prevent the package from being different every time you run it
-    home-manager.flake = inputs.home-manager;
-    nixpkgs.flake = inputs.nixpkgs;
-    nur.flake = inputs.nur;
+  nix = {
+    registry = {
+      # This setting is important because it makes things like:
+      # `nix run nixpkgs#some-package` makes it use the same reference of packages as in your 
+      # flake.lock, which helps prevent the package from being different every time you run it
+      home-manager.flake = inputs.home-manager;
+      nixpkgs.flake = inputs.nixpkgs;
+      nur.flake = inputs.nur;
+    };
   };
 
   nixpkgs.config = {
