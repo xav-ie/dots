f1bd4f73b0045b63ad366fa200f82287058540a2
Xavier Ruiz <github@xav.ie>
2024-04-23 08:20:09 -0400

    format

--- modules/home-manager/linux.nix
+++ modules/home-manager/linux.nix
@@ -1,100 +1,108 @@
-{ pkgs, lib, inputs, ... }:
-let merge = lib.foldr (a: b: a // b) { };
-in {
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+let
+  merge = lib.foldr (a: b: a // b) { };
+in
+{
   home = {
-    packages = (with pkgs; [
-      ################################
-      # in triage - try to minimize this list
-      ################################
-      slack
-      asciinema # record shell sessions and share easily
-      age # the new PGP
-      blesh # bash extensions
-      cliphist
-      clipboard-jh # a really awesome clipboard
-      # cudaPackages.cuda_cccl # I wish hardware acceleration would work :/
-      # cudaPackages.cudatoolkit
-      # cudaPackages.cudnn
-      himalaya # email
-      hstr
-      manix
-      nodePackages."webtorrent-cli"
-      xidel # like jq but for html and much more advanced.
-      # required by mpvScripts.webtorrent-mpv-hook
-      pciutils
-      pinentry-gnome3 # I wish I could figure out pinentry-rofi but it does not work
-      # prusa-slicer                # does not launch currently
-      python312Packages."adblock"
-      rofi-rbw # bitwarden cli wrapper
-      rbw # unnofficial bitwarden client
-      # slack                       # does not launch currently
-      sops # secrets manager? idk... seems like an extension to age and
-      # other encrypters that allows you to just encrypt part of the
-      # file instead of the whole thing... IDK the real use for #that
-      # tldr # barely working due to it not having many entries
-      xdg-utils # ????
-      wf-recorder
-      wtype # xdotool for wayland; used as part of rofi-rbw for typing
-      # passwords out
-      yt-dlp # better yt-dl
-      zoom-us
-      # https://github.com/marionebl/svg-term-cli
-      # allows asciinema recordings to be exported to svg... this could be pretty indespensable if
-      # you would like ANSI escape sequences to be interpreted by GH
-      ################################
-      # awesome dev tools
-      ################################
-      lazygit # easy git tui
-      #neovim # the one and only
-      ################################
-      # universal utils
-      ################################
-      cmake
-      file # magic number reader
-      gcc
-      # gnumake # provides `make`, which should already be provided?
-      #ninja
-      vim
-      wget
-      ################################
-      # user programs
-      ################################
-      bitwarden
-      chromium
-      discord
-      google-chrome
-      mpv # video player
-      networkmanagerapplet
-      noisetorch # noise filter
-      openrgb # pc rgb control
-      pavucontrol # audio mixer
-      nerdfonts
-      # (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; })
-      playerctl # play, pause, next
-      pulseaudio # provides pactl for volume control
-      # qutebrowser
-      sioyek # vimified pdf viewer
-      ################################
-      # hyprland
-      ################################
-      cava
-      libnotify
-      libva
-      libva-utils # hardware video acceleration
-      polkit_gnome # just a GUI askpass
-      rofi-wayland
-      swayidle
-      swaylock
-      swaynotificationcenter
-      swww
-      waypipe
-      wl-clipboard
-      (waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-        hyprlandSupport = true;
-      }))
-
-    ]) ++ [ inputs.hyprland-contrib.packages."x86_64-linux".grimblast ];
+    packages =
+      (with pkgs; [
+        ################################
+        # in triage - try to minimize this list
+        ################################
+        slack
+        asciinema # record shell sessions and share easily
+        age # the new PGP
+        blesh # bash extensions
+        cliphist
+        clipboard-jh # a really awesome clipboard
+        # cudaPackages.cuda_cccl # I wish hardware acceleration would work :/
+        # cudaPackages.cudatoolkit
+        # cudaPackages.cudnn
+        himalaya # email
+        hstr
+        manix
+        nodePackages."webtorrent-cli"
+        xidel # like jq but for html and much more advanced.
+        # required by mpvScripts.webtorrent-mpv-hook
+        pciutils
+        pinentry-gnome3 # I wish I could figure out pinentry-rofi but it does not work
+        # prusa-slicer                # does not launch currently
+        python312Packages."adblock"
+        rofi-rbw # bitwarden cli wrapper
+        rbw # unnofficial bitwarden client
+        # slack                       # does not launch currently
+        sops # secrets manager? idk... seems like an extension to age and
+        # other encrypters that allows you to just encrypt part of the
+        # file instead of the whole thing... IDK the real use for #that
+        # tldr # barely working due to it not having many entries
+        xdg-utils # ????
+        wf-recorder
+        wtype # xdotool for wayland; used as part of rofi-rbw for typing
+        # passwords out
+        yt-dlp # better yt-dl
+        zoom-us
+        # https://github.com/marionebl/svg-term-cli
+        # allows asciinema recordings to be exported to svg... this could be pretty indespensable if
+        # you would like ANSI escape sequences to be interpreted by GH
+        ################################
+        # awesome dev tools
+        ################################
+        lazygit # easy git tui
+        #neovim # the one and only
+        ################################
+        # universal utils
+        ################################
+        cmake
+        file # magic number reader
+        gcc
+        # gnumake # provides `make`, which should already be provided?
+        #ninja
+        vim
+        wget
+        ################################
+        # user programs
+        ################################
+        bitwarden
+        chromium
+        discord
+        google-chrome
+        mpv # video player
+        networkmanagerapplet
+        noisetorch # noise filter
+        openrgb # pc rgb control
+        pavucontrol # audio mixer
+        nerdfonts
+        # (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; })
+        playerctl # play, pause, next
+        pulseaudio # provides pactl for volume control
+        # qutebrowser
+        sioyek # vimified pdf viewer
+        ################################
+        # hyprland
+        ################################
+        cava
+        libnotify
+        libva
+        libva-utils # hardware video acceleration
+        polkit_gnome # just a GUI askpass
+        rofi-wayland
+        swayidle
+        swaylock
+        swaynotificationcenter
+        swww
+        waypipe
+        wl-clipboard
+        (waybar.overrideAttrs (oldAttrs: {
+          mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+          hyprlandSupport = true;
+        }))
+      ])
+      ++ [ inputs.hyprland-contrib.packages."x86_64-linux".grimblast ];
     # The state version is required and should stay at the version you
     # originally installed.
     stateVersion = "23.11";
@@ -191,14 +199,22 @@ in {
 
   systemd.user = {
     services.ollama-server = {
-      Unit = { Description = "Run ollama server"; };
-      Install = { WantedBy = [ "default.target" ]; };
-      Service = { ExecStart = "${pkgs.ollama}/bin/ollama serve"; };
+      Unit = {
+        Description = "Run ollama server";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.ollama}/bin/ollama serve";
+      };
     };
 
     # Zoom and Slack love to "helpfully" stay open even when I kill them through UI
     services.kill-spyware = {
-      Unit = { Description = "Stop spyware"; };
+      Unit = {
+        Description = "Stop spyware";
+      };
       Service = {
         Type = "oneshot";
         ExecStart = ./dotfiles/kill-spyware.sh;
@@ -207,17 +223,23 @@ in {
       };
     };
     timers.kill-spyware = {
-      Unit = { Description = "Stop spyware after working hours"; };
+      Unit = {
+        Description = "Stop spyware after working hours";
+      };
       Timer = {
         Unit = "kill-spyware.service";
         OnCalendar = "18:00";
         Persistent = true;
       };
-      Install = { WantedBy = [ "timers.target" ]; };
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
     };
 
     services.start-work = {
-      Unit = { Description = "Start work programs"; };
+      Unit = {
+        Description = "Start work programs";
+      };
       Service = {
         Type = "oneshot";
         ExecStart = ./dotfiles/start-work.sh;
@@ -225,13 +247,17 @@ in {
       };
     };
     timers.start-work = {
-      Unit = { Description = "Start work programs every weekday at 9am"; };
+      Unit = {
+        Description = "Start work programs every weekday at 9am";
+      };
       Timer = {
         Unit = "start-work.service";
         OnCalendar = "Mon..Fri 09:00";
         Persistent = true;
       };
-      Install = { WantedBy = [ "timers.target" ]; };
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
     };
   };
 
