5d5fab7407dacebe78e729971d92aa33df204883
Xavier Ruiz <github@xav.ie>
2024-04-23 17:07:42 -0400

    config(substiters): allow x as a substituter

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    trusted-users = [ "x" ];
   };
   inputs = {
     # TODO: figure out how to use from misterio and vimjoyer
