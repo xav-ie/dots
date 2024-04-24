e93b9f7f0c0a6198c7fbe5a6dc586916e4d022ca
Xavier Ruiz <github@xav.ie>
2024-04-24 06:55:56 -0400

    refactor(firefox): move

--- modules/home-manager/linux.nix
+++ modules/home-manager/linux.nix
@@ -1,13 +1,6 @@
+{ pkgs, inputs, ... }:
 {
-  pkgs,
-  lib,
-  inputs,
-  ...
-}:
-let
-  merge = lib.foldr (a: b: a // b) { };
-in
-{
+  imports = [ ./programs/firefox ];
   home = {
     packages =
       (with pkgs; [
@@ -107,70 +100,6 @@ in
     lazygit.enable = true; # easy git tui
     himalaya.enable = true; # email
     sioyek.enable = true; # vimified pdf viewer
-    firefox = {
-      enable = true;
-      profiles.x = {
-        id = 0;
-        isDefault = true;
-        settings = merge [
-          (import ./programs/firefox/annoyances.nix)
-          (import ./programs/firefox/settings.nix)
-        ];
-        userChrome = # css
-          ''
-            /* ########  Sidetabs Styles  ######### */
-
-            /* ~~~~~~~~ Autohiding styles ~~~~~~~~~ */
-            :root {
-             --sidebar-hover-width: 36px;
-             --sidebar-visible-width: 190px;
-             --sidebar-debounce-delay: 50ms;
-
-            }
-            #sidebar-box {
-             display: grid !important;
-             min-width: var(--sidebar-hover-width) !important;
-             max-width: var(--sidebar-hover-width) !important;
-             overflow: visible !important;
-             height: 100% !important;
-             min-height: 100% !important;
-             max-height: 100% !important;
-            }
-            #sidebar {
-             height: 100% !important;
-             width: var(--sidebar-hover-width) !important;
-             z-index: 200 !important;
-             position: absolute !important;
-             transition: width 150ms var(--sidebar-debounce-delay) ease !important;
-             min-width: 0 !important;
-            }
-            #sidebar:hover {
-             width: var(--sidebar-visible-width) !important;
-            }
-            /* ~~~~~~~~ Hidden elements styles ~~~~~~~~~ */
-            #TabsToolbar {
-            	display: none !important;
-            }
-            #titlebar {
-            	display: none !important;
-            }
-            #sidebar-header {
-            	display: none !important;
-            }
-            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-            /* #################################### */
-          '';
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          bitwarden
-          ublock-origin
-          vimium-c
-          sidebartabs
-          newtab-adapter
-          videospeed
-        ];
-      };
-    };
     # idk what's happening. It has not been working for awhile
     # wezterm = {
     #   # guess this does not work with the flake version
