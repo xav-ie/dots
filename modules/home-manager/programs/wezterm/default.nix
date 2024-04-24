0cbc6d56d804d8633a3d962e8d91d33e6452bab6
xav-ie <github@xav.ie>
2024-04-24 03:30:13 -0400

    refactor(wezterm): move

---
+++ modules/home-manager/programs/wezterm/default.nix
@@ -0,0 +1,49 @@
+{ ... }:
+{
+  programs = {
+    wezterm = {
+      enable = true;
+      extraConfig = # lua
+        ''
+          -- Pull in the wezterm API
+          local wezterm = require 'wezterm'
+          local mux = wezterm.mux
+
+          wezterm.on("gui-startup", function()
+            local tab, pane, window = mux.spawn_window{}
+            window:gui_window():maximize()
+          end)
+
+          -- This table will hold the configuration.
+          local config = {}
+
+          -- In newer versions of wezterm, use the config_builder which will
+          -- help provide clearer error messages
+          if wezterm.config_builder then
+            config = wezterm.config_builder()
+          end
+
+          -- This is where you actually apply your config choices
+
+          -- For example, changing the color scheme:
+          config = {
+            window_background_opacity = 0.95,
+            macos_window_background_blur = 0,
+            color_scheme = 'Argonaut',
+            window_decorations = "RESIZE",
+            enable_tab_bar = false,
+            -- use_fancy_tab_bar = false
+            window_padding = {
+              left = 0,
+              right = 0,
+              top = 0,
+              bottom = 0,
+            },
+          }
+
+          -- and finally, return the configuration to wezterm
+          return config
+        '';
+    };
+  };
+}
