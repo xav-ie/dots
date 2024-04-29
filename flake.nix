57f373e765131039e2667b7b69f94834f1d129cb
Xavier Ruiz <github@xav.ie>
2024-04-29 08:54:54 -0400

    fix(extra-trusted-substituters): dang

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     # https://github.com/NixOS/nix/issues/6672#issuecomment-1921937241
     extra-trusted-substituters = [
       "https://nix-community.cachix.org"
-      "https://devenv.cachix.or"
+      "https://devenv.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
