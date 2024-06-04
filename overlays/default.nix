d0594491200e51528a2ad1a0d0545d8645cdad83
xav-ie <github@xav.ie>
2024-06-04 14:22:14 -0400

    format

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,17 +1,18 @@
-{ inputs, outputs }: {
+{ inputs, outputs }:
+{
   nur = inputs.nur.overlay;
   # For every flake input, aliases 'pkgs.inputs.${flake}' to
   # 'inputs.${flake}.packages.${pkgs.system}' or
   # 'inputs.${flake}.legacyPackages.${pkgs.system}'
   flake-inputs = final: _: {
-    inputs = builtins.mapAttrs
-      (_: flake:
-        let
-          legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
-          packages = ((flake.packages or { }).${final.system} or { });
-        in
-        if legacyPackages != { } then legacyPackages else packages)
-      inputs;
+    inputs = builtins.mapAttrs (
+      _: flake:
+      let
+        legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
+        packages = ((flake.packages or { }).${final.system} or { });
+      in
+      if legacyPackages != { } then legacyPackages else packages
+    ) inputs;
   };
 
   # Adds my custom packages
@@ -23,16 +24,17 @@
     generate-kaomoji = inputs.generate-kaomoji.packages.${final.system}.default;
     ollama = inputs.ollama.packages.${final.system}.default;
     mpv = prev.mpv.override {
-      scripts = with final.mpvScripts;
+      scripts =
+        with final.mpvScripts;
         [
           autoload # autoloads entries before and after current entry
           mpv-playlistmanager # resolves url titles, SHIFT+ENTER for playlist
           quality-menu # control video quality on the fly
           webtorrent-mpv-hook # extends mpv to handle magnet URLs
-        ] ++
-        # extends mpv to be controllable with MPD
-        final.lib.optional (final.system == "x86_64-linux")
-          final.mpvScripts.mpris;
+        ]
+        ++
+          # extends mpv to be controllable with MPD
+          final.lib.optional (final.system == "x86_64-linux") final.mpvScripts.mpris;
     };
     zjstatus = inputs.zjstatus.packages.${final.system}.default;
   };
