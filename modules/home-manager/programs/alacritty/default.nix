9ac7cebbefa76a21f2fb3363028c6415a6465d93
Xavier Ruiz <github@xav.ie>
2024-06-07 17:24:25 -0400

    config(alacritty): match other terms

--- modules/home-manager/programs/alacritty/default.nix
+++ modules/home-manager/programs/alacritty/default.nix
@@ -9,19 +9,20 @@
         # font.normal.family = "MonaspiceAr NF Medium";
         # not available yet, awaiting full test
         # font.normal.family = "Cartograph Nerd Font";
-        font.normal.family = "FiraCode Nerd Font Ret";
+        font.normal.family = "Maple Mono";
         # Hack has better spacing and numbers than Fira, 
         # but has worse special characters. Fira has some cool letters but both
         # have letter spacing problems "ma" "wa" both look bad when not italic
         # font.normal.family = "Hack Nerd Font";
         font.size = 14;
         window = {
-          #decorations = "Transparent";
-          opacity = 0.9;
+          decorations = "None";
+          opacity = 0.8;
           blur = true;
+          # startup_mode = "SimpleFullscreen";
           #option_as_alt = "Both";
         };
-        import = [ pkgs.alacritty-theme.iterm ];
+        import = [ pkgs.alacritty-theme.hyper ];
         # import = [ pkgs.alacritty-theme.papercolor_light ];
         keyboard.bindings = [
           {
@@ -50,7 +51,7 @@
             key = "Tab";
             mods = "Alt";
             command = {
-              program = "zellij";
+              program = "/etc/profiles/per-user/xavierruiz/bin/zellij";
               args = [
                 "action"
                 "focus-next-pane"
@@ -61,10 +62,59 @@
             key = "Tab";
             mods = "Alt|Shift";
             command = {
-              program = "zellij";
+              program = "/etc/profiles/per-user/xavierruiz/bin/zellij";
               args = [
                 "action"
                 "focus-previous-pane"
+              ];
+            };
+          }
+          # TODO: make cross-platform
+          {
+            key = "h";
+            mods = "Alt";
+            command = {
+              program = "/etc/profiles/per-user/xavierruiz/bin/zellij";
+              args = [
+                "action"
+                "move-focus-or-tab"
+                "left"
+              ];
+            };
+          }
+          {
+            key = "j";
+            mods = "Alt";
+            command = {
+              program = "/etc/profiles/per-user/xavierruiz/bin/zellij";
+              args = [
+                "action"
+                "move-focus"
+                "down"
+              ];
+            };
+          }
+          {
+            key = "k";
+            mods = "Alt";
+            command = {
+              program = "/etc/profiles/per-user/xavierruiz/bin/zellij";
+              args = [
+                "action"
+                "move-focus"
+                "up"
+              ];
+            };
+          }
+          {
+            key = "l";
+            mods = "Alt";
+            command = {
+              program = "/etc/profiles/per-user/xavierruiz/bin/zellij";
+              args = [
+                "action"
+                "move-focus-or-tab"
+                "right"
               ];
             };
           }
