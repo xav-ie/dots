69d291b6813d5bfe7dcc6b3bc1416908bd4e5e61
Xavier Ruiz <github@xav.ie>
2024-06-19 14:42:06 -0400

    fix(waybar): follow master branch flake
    They don't have fast release process for bugfixes, so I must follow the flake to ensure stability.

--- modules/home-manager/programs/waybar/default.nix
+++ modules/home-manager/programs/waybar/default.nix
@@ -1,18 +1,14 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 {
   # https://github.com/elythh/nixdots/blob/58db47f160c219c3e2a9630651dfd9aab0408b1a/modules/home/opt/wayland/services/swaync/default.nix
   programs = {
     waybar = {
       enable = true;
+      package = inputs.waybar.packages.${pkgs.system}.default;
       settings = {
         mainBar = import ./config.nix;
       };
       style = builtins.readFile (./. + "/style.css");
     };
   };
-  # not sure if I still need this
-  # (waybar.overrideAttrs (oldAttrs: {
-  #   mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-  #   hyprlandSupport = true;
-  # }))
 }
