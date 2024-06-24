f8d7e7f09fe39baf9698e2ceaa25db9a9ea0a6b0
Xavier Ruiz <github@xav.ie>
2024-06-24 04:23:07 -0400

    refactor(organize): move giant mess into files

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, inputs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
 {
   nix = {
     # https://nixos.wiki/wiki/Storage_optimization
@@ -9,14 +14,15 @@
       # dates = "weekly";
       options = "--delete-older-than 30d";
     };
-    registry = {
-      # This setting is important because it makes things like:
-      # `nix run nixpkgs#some-package` makes it use the same reference of packages as in your 
-      # flake.lock, which helps prevent the package from being different every time you run it
-      home-manager.flake = inputs.home-manager;
-      nixpkgs.flake = inputs.nixpkgs;
-      nur.flake = inputs.nur;
-    };
+
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
     settings = {
       # just run this every once in a while... auto-optimization slows down evaluation
       auto-optimise-store = false;
@@ -43,23 +49,4 @@
   nixpkgs.config = {
     allowUnfree = true;
   };
-
-  environment.systemPackages = (
-    with pkgs;
-    [
-      # TODO: put these in a better place
-      cache-command
-      ff
-      generate-kaomoji
-      is-sshed
-      j
-      jira-list
-      jira-task-list
-      notify
-      nvim
-      searcher
-      uair-toggle-and-notify
-      zellij-tab-name-update
-    ]
-  );
 }
