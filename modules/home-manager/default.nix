5f2688ab08715c773ddd141a5b283c581cae52c7
Xavier Ruiz <github@xav.ie>
2024-06-24 04:23:07 -0400

    refactor(organize): move giant mess into files

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,14 +2,34 @@
 {
   imports = [
     ./programs/alacritty
+    # ./programs/atuin
+    ./programs/bat
+    ./programs/direnv
+    ./programs/eza
+    # ./programs/firefox
     ./programs/git
+    ./programs/kitty
     ./programs/lf
+    ./programs/moar
     ./programs/neovide
+    ./programs/nvim
     ./programs/starship
+    # ./programs/swaynotificationcenter
+    # ./programs/waybar
     ./programs/wezterm
     ./programs/zellij
     ./programs/zsh
   ];
+  programs = {
+    btop.enable = true;
+    fzf.enable = true;
+    jq.enable = true;
+    mpv.enable = true;
+    ripgrep.enable = true;
+    thefuck.enable = true;
+    watson.enable = true;
+    zoxide.enable = true;
+  };
   home = {
     packages =
       with pkgs;
@@ -27,106 +47,42 @@
           '';
         }))
         magic-wormhole-rs # send files easily
-        moar # the best pager
         neovide
         uair
         unzip
+        tldr
         tree
         wget
         zip
+      ]
+      # custom packages
+      ++ [
+        cache-command
+        ff
+        generate-kaomoji
+        is-sshed
+        j
+        jira-list
+        jira-task-list
+        notify
+        nvim
+        searcher
+        uair-toggle-and-notify
+        zellij-tab-name-update
       ]
       ++ [ ollama ];
     # The state version is required and should stay at the version you
     # originally installed.
     stateVersion = "23.11";
     sessionVariables = {
-      BROWSER = "firefox";
       EDITOR = "$HOME/Projects/xnixvim/result/bin/nvim";
       LANG = "en_US.UTF-8";
       LC_ALL = "en_US.UTF-8";
-      # causes bug if set. dont do it!
-      BAT_PAGER = "";
-      PAGER = ''bat -p --pager=\"moar -quit-if-one-screen\" --terminal-width=$(expr $COLUMNS - 4)'';
-      MOAR = "-quit-if-one-screen";
-      TERMINAL = "kitty";
-      # get more colors
-      HSTR_CONFIG = "hicolor";
-      # leading space hides commands from history
-      HISTCONTROL = "ignorespace";
-      # increase history file size (default is 500)
-      HISTFILESIZE = 10000;
-      SOMETHING_RANDOM = 12;
+      HSTR_CONFIG = "hicolor"; # get more colors
+      HISTCONTROL = "ignorespace"; # leading space hides commands from history
+      HISTFILESIZE = 100000; # increase history file size (default is 500)
       PATH = "$HOME/.config/scripts/:$PATH";
     };
-  };
-  programs = {
-    # atuin = {
-    #   enable = true;
-    #   settings.style = "compact";
-    # };
-    bat = {
-      enable = true;
-      config = {
-        theme = "ansi";
-        pager = "moar -quit-if-one-screen";
-        paging = "auto";
-        style = "plain";
-        wrap = "character";
-      };
-    };
-    btop.enable = true;
-    direnv = {
-      enable = true;
-      # very important, allows caching of build-time deps
-      nix-direnv.enable = true;
-    };
-    eza = {
-      enable = true;
-      git = true;
-      icons = true;
-    };
-    fzf.enable = true;
-    jq.enable = true;
-    kitty = {
-      enable = true;
-      keybindings = {
-        "alt+h" = ''send_text all \x1bh'';
-        "alt+j" = ''send_text all \x1bj'';
-        "alt+k" = ''send_text all \x1bk'';
-        "alt+l" = ''send_text all \x1bl'';
-      };
-      settings = {
-        background = "#0a0a0f";
-        background_blur = 10;
-        background_opacity = "0.80";
-        clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
-        copy_on_select = "yes";
-        cursor = "#ff0000";
-        font_family = "Maple Mono";
-        font_size = "13.0";
-        hide_window_decorations = "yes";
-        macos_quit_when_last_window_closed = "yes";
-      };
-
-      # kitty +list-fonts --psnames | grep Maple
-      extraConfig =
-        let
-          mapleFontFeatures = "+cv01 +cv02 +cv04 +ss01 +ss02 +ss03 +ss04 +ss05";
-        in
-        ''
-          font_features MapleMono-Bold ${mapleFontFeatures}
-          font_features MapleMono-BoldItalic ${mapleFontFeatures}
-          font_features MapleMono-Italic ${mapleFontFeatures}
-          font_features MapleMono-Light ${mapleFontFeatures}
-          font_features MapleMono-LightItalic ${mapleFontFeatures}
-          font_features MapleMono-Regular ${mapleFontFeatures}
-        '';
-    };
-    mpv.enable = true;
-    ripgrep.enable = true;
-    thefuck.enable = true;
-    watson.enable = true;
-    zoxide.enable = true;
   };
   home.file.".inputrc".source = ./dotfiles/inputrc;
   home.file.".config/scripts/localip".source = ./dotfiles/localip;
