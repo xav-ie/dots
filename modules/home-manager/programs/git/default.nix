18b3ef131cec0ec7fb43baf601d9da755bf5a3ad
Xavier Ruiz <github@xav.ie>
2024-06-25 17:36:38 -0400

    refactor(git): new commands and adjustments
    cm - new command 'commit -m'
    sh - 'git show --patch-with-stat'; use this for better 'git show'
    shove - use this instead of force; an alias to 'push --force-with-lease'
    unstage - 'git restore --staged .'
    
    some alphabetization
    some refactoring to make 'git d' and 'git dc' easier to read
    'd/dc' now also include stat

--- modules/home-manager/programs/git/default.nix
+++ modules/home-manager/programs/git/default.nix
@@ -5,48 +5,56 @@
       enable = true;
       userName = "xav-ie";
       # userEmail = "github@xav.ie";
-      aliases = {
-        bb = "!${./betterbranch.sh}";
-        bblame = "blame -w -C -C -C";
-        cam = "commit -am";
-        c = "commit";
-        dc = "diff --cached --ignore-all-space --ignore-space-at-eol --ignore-space-change --ignore-blank-lines -- . ':(exclude)*package-lock.json' -- . ':(exclude)*yarn.lock'";
-        d = "diff --ignore-all-space --ignore-space-at-eol --ignore-space-change --ignore-blank-lines -- . ':(exclude)*package-lock.json' -- . ':(exclude)*yarn.lock'";
-        graph = "log --graph --pretty=tformat:'%C(bold blue)%h%Creset %s %C(bold green)%d%Creset %C(blue)<%an>%Creset %C(dim cyan)%cr' --abbrev-commit --decorate";
-        main = # sh
-          "!(git fetch && git fetch --tags && git checkout -B main origin/main)";
-        p = "push";
-        pr = # sh
-          ''
-            !(GH_FORCE_TTY=100% gh pr list | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' --preview-window up --header-lines 3 | awk '{print $1}' | xargs -r gh pr checkout)
-          '';
-        rmc = "rm --cached";
-        s = "status";
-        staash = "stash --all";
-        # git log -L :functionName:/path/to/file
-        # git blame -L :functionName:/path/to/file
-        # git log -S your_regex -p 
-        # git reflog <- idk what this does other than show history
-        # "So You Think You Know Git - Part 2":
-        # https://www.youtube.com/watch?v=Md44rcw13k4
-        # Helpful hooks
-        ## Commit Stuff
-        # - pre-commit
-        # - prepare-commit-msg
-        # - commit-msg
-        # - post-commit
-        ## Rewriting stuff
-        # - pre-rebase
-        # - post-rewrite
-        ## Merging Stuff
-        # - post-merge
-        # - pre-merge-commit
-        ## Switching/Pushing Stuff
-        # - post-checkout
-        # - reference-transaction
-        # - pre-push
-        # He recommends `pre-commit` binary
-      };
+      aliases =
+        let
+          diffTweaks = "--ignore-all-space --ignore-space-at-eol --ignore-space-change --ignore-blank-lines --patch-with-stat -- . ':(exclude)*lock.json' -- . ':(exclude)*.lock'";
+        in
+        {
+          bb = "!${./betterbranch.sh}";
+          blame-better = "blame -w -C -C -C";
+          cam = "commit -am";
+          c = "commit";
+          cm = "commit -m";
+          d = "diff ${diffTweaks}";
+          dc = "diff --cached ${diffTweaks}";
+          graph = "log --graph --pretty=tformat:'%C(bold blue)%h%Creset %s %C(bold green)%d%Creset %C(blue)<%an>%Creset %C(dim cyan)%cr' --abbrev-commit --decorate";
+          main = # sh
+            "!(git fetch && git fetch --tags && git checkout -B main origin/main)";
+          p = "push";
+          pr = # sh
+            ''
+              !(GH_FORCE_TTY=100% gh pr list | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' --preview-window up --header-lines 3 | awk '{print $1}' | xargs -r gh pr checkout)
+            '';
+          rmc = "rm --cached";
+          s = "status";
+          sh = "show --patch-with-stat";
+          shove = "git push --force-with-lease";
+          stash-all = "stash --all";
+          unstage = "restore --staged .";
+          # git log -L :functionName:/path/to/file
+          # git blame -L :functionName:/path/to/file
+          # git log -S your_regex -p 
+          # git reflog <- idk what this does other than show history
+          # "So You Think You Know Git - Part 2":
+          # https://www.youtube.com/watch?v=Md44rcw13k4
+          # Helpful hooks
+          ## Commit Stuff
+          # - pre-commit
+          # - prepare-commit-msg
+          # - commit-msg
+          # - post-commit
+          ## Rewriting stuff
+          # - pre-rebase
+          # - post-rewrite
+          ## Merging Stuff
+          # - post-merge
+          # - pre-merge-commit
+          ## Switching/Pushing Stuff
+          # - post-checkout
+          # - reference-transaction
+          # - pre-push
+          # He recommends `pre-commit` binary
+        };
       # attributes = {
       # };
       # I am guessing this option sets up the options I set in extraConfig
@@ -82,6 +90,33 @@
         };
         branch.sort = "-committerdate";
         column.ui = "auto";
+        fetch.writeCommitGraph = true;
+        diff = {
+          colorMoved = "default";
+        };
+        # configured by delta.enable=true
+        # delta = {
+        #   navigate = true;
+        #   line-numbers = true;
+        #   true-color = "always";
+        # };
+        "includeIf \"gitdir:~/\"" = {
+          path = "~/.config/git/config.default";
+        };
+        "includeIf \"gitdir:~/Outsmartly/\"" = {
+          path = "~/.config/git/config.work";
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        interactive = {
+          # configured by delta.enable=true
+          # this is used for diff patches
+          # diffFilter = "delta";
+        };
+        merge = {
+          conflictstyle = "diff3";
+        };
         # This is needed so programs like Fugitive will use delta
         pager = {
           blame = "delta";
@@ -99,36 +134,8 @@
         # https://andrewlock.net/working-with-stacked-branches-in-git-is-easier-with-update-refs/
         # to temporarily turn off, --no-update-refs
         rebase.updateRefs = true;
-        rerere.enabled = true;
-        fetch.writeCommitGraph = true;
-        # remote.origin.fetch = "+refs/pull/*:refs/remotes/origin/pull/*";
         remote.origin.fetch = "+refs/pull/*/head:refs/remotes/origin/pr/*";
-        interactive = {
-          # configured by delta.enable=true
-          # this is used for diff patches
-          # diffFilter = "delta";
-        };
-        # configured by delta.enable=true
-        # delta = {
-        #   navigate = true;
-        #   line-numbers = true;
-        #   true-color = "always";
-        # };
-        init = {
-          defaultBranch = "main";
-        };
-        merge = {
-          conflictstyle = "diff3";
-        };
-        diff = {
-          colorMoved = "default";
-        };
-        "includeIf \"gitdir:~/\"" = {
-          path = "~/.config/git/config.default";
-        };
-        "includeIf \"gitdir:~/Outsmartly/\"" = {
-          path = "~/.config/git/config.work";
-        };
+        rerere.enabled = true;
       };
     };
   };
