a028347002ff68f5cf0c1552f6b940ee4d661bcd
Xavier Ruiz <github@xav.ie>
2024-08-25 08:34:48 -0400

    docs(direnv)

--- modules/home-manager/programs/direnv/default.nix
+++ modules/home-manager/programs/direnv/default.nix
@@ -1,8 +1,10 @@
 { ... }:
 {
+  # https://direnv.net/
   programs.direnv = {
     enable = true;
     # very important, allows caching of build-time deps
+    # https://github.com/nix-community/nix-direnv
     nix-direnv.enable = true;
   };
 }
