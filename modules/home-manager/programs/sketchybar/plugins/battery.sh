c17b483dd27cdf80f613658b13348bf8f64bd1a6
Xavier Ruiz <github@xav.ie>
2024-10-07 08:56:02 -0400

    fix(sketchybar): use sf-symbols and add native menu-bar icons

--- modules/home-manager/programs/sketchybar/plugins/battery.sh
+++ modules/home-manager/programs/sketchybar/plugins/battery.sh
@@ -7,60 +7,60 @@ if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-if [[ "$CHARGING" != "" ]]; then
-  case "${PERCENTAGE}" in
-    100) ICON="󱈏"
-    ;;
-    9[0-9]) ICON="󰂋"
-    ;;
-    8[0-9]) ICON="󰂊"
-    ;;
-    7[0-9]) ICON="󰢞"
-    ;;
-    6[0-9]) ICON="󰂉"
-    ;;
-    5[0-9]) ICON="󰢝"
-    ;;
-    4[0-9]) ICON="󰂈"
-    ;;
-    3[0-9]) ICON="󰂇"
-    ;;
-    2[0-9]) ICON="󰂆"
-    ;;
-    1[0-9]) ICON="󰢜"
-    ;;
-    *) ICON="󰢟"
-  esac
-  ICON_SIZE=25.0
-  ICON_PADDING_RIGHT=0
-else
-  case "${PERCENTAGE}" in
-    100) ICON="󰁹"
-    ;;
-    9[0-9]) ICON="󰂂"
-    ;;
-    8[0-9]) ICON="󰂁"
-    ;;
-    7[0-9]) ICON="󰂀"
-    ;;
-    6[0-9]) ICON="󰁿"
-    ;;
-    5[0-9]) ICON="󰁾"
-    ;;
-    4[0-9]) ICON="󰁽"
-    ;;
-    3[0-9]) ICON="󰁼"
-    ;;
-    2[0-9]) ICON="󰁻"
-    ;;
-    1[0-9]) ICON="󰁺"
-    ;;
-    *) ICON="󰂎"
-  esac
-  ICON_SIZE=14.0
-  ICON_PADDING_RIGHT=7
-fi
+# if [[ "$CHARGING" != "" ]]; then
+#   case "${PERCENTAGE}" in
+#     100) ICON="󱈏"
+#     ;;
+#     9[0-9]) ICON="󰂋"
+#     ;;
+#     8[0-9]) ICON="󰂊"
+#     ;;
+#     7[0-9]) ICON="󰢞"
+#     ;;
+#     6[0-9]) ICON="󰂉"
+#     ;;
+#     5[0-9]) ICON="󰢝"
+#     ;;
+#     4[0-9]) ICON="󰂈"
+#     ;;
+#     3[0-9]) ICON="󰂇"
+#     ;;
+#     2[0-9]) ICON="󰂆"
+#     ;;
+#     1[0-9]) ICON="󰢜"
+#     ;;
+#     *) ICON="󰢟"
+#   esac
+#   ICON_SIZE=25.0
+#   ICON_PADDING_RIGHT=0
+# else
+#   case "${PERCENTAGE}" in
+#     100) ICON="󰁹"
+#     ;;
+#     9[0-9]) ICON="󰂂"
+#     ;;
+#     8[0-9]) ICON="󰂁"
+#     ;;
+#     7[0-9]) ICON="󰂀"
+#     ;;
+#     6[0-9]) ICON="󰁿"
+#     ;;
+#     5[0-9]) ICON="󰁾"
+#     ;;
+#     4[0-9]) ICON="󰁽"
+#     ;;
+#     3[0-9]) ICON="󰁼"
+#     ;;
+#     2[0-9]) ICON="󰁻"
+#     ;;
+#     1[0-9]) ICON="󰁺"
+#     ;;
+#     *) ICON="󰂎"
+#   esac
+#   ICON_SIZE=14.0
+#   ICON_PADDING_RIGHT=7
+# fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" icon.font.size=$ICON_SIZE icon.padding_right=$ICON_PADDING_RIGHT label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon.padding_left=0 icon.padding_right=4 label="${PERCENTAGE}%"
--- modules/home-manager/programs/sketchybar/plugins/volume.sh
+++ modules/home-manager/programs/sketchybar/plugins/volume.sh
@@ -7,13 +7,15 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
 
   case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾" ICON_SIZE=20.0 ICON_PADDING_RIGHT=0
+    7[5-9]|8[0-9]|9[0-9]|100) ICON="􀊩" ICON_SIZE=14.0 ICON_PADDING_RIGHT=0 # three bars
     ;;
-    [3-5][0-9]) ICON="󰖀" ICON_SIZE=16.0 ICON_PADDING_RIGHT=2
+    5[0-9]|6[0-9]|7[0-4]) ICON="􀊧" ICON_SIZE=14.0 ICON_PADDING_RIGHT=3 # two bars
     ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿" ICON_SIZE=10.0 ICON_PADDING_RIGHT=6
+    2[5-9]|3[0-9]|4[0-9]) ICON="􀊥" ICON_SIZE=14.0 ICON_PADDING_RIGHT=6 # one bar
     ;;
-    *) ICON="󰖁" ICON_SIZE=20.0 ICON_PADDING_RIGHT=0
+    [1-9]|1[0-9]|2[0-4]) ICON="􀊡" ICON_SIZE=14.0 ICON_PADDING_RIGHT=10 # no bars
+    ;;
+    0) ICON="􀊣" ICON_SIZE=14.0 ICON_PADDING_RIGHT=6 # muted
   esac
 
   sketchybar --set "$NAME" icon="$ICON" icon.font.size=$ICON_SIZE icon.padding_right=$ICON_PADDING_RIGHT label="$VOLUME%"
