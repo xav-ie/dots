57ae21e22a44a1c3597c592d4f189f95ec4ceaac
Xavier Ruiz <github@xav.ie>
2024-10-20 02:03:29 -0400

    fix(canonicalize): stella host config
    I find non-canonicalized configs confusing/annoying
    because it is more to think about and more work to do
    for when you do want to factor into a canonical module.
    Just make them canonical by default!

--- modules/home-manager/programs/sketchybar/default.nix
+++ modules/home-manager/programs/sketchybar/default.nix
@@ -15,29 +15,31 @@ let
   '';
 in
 {
-  home = {
-    packages = [ pkgs.sketchybar ];
+  config = {
+    home = {
+      packages = [ pkgs.sketchybar ];
 
-    file = {
-      ".config/sketchybar/sketchybarrc".source = mkSketchybarScript "sketchybarrc" ./sketchybarrc;
-      ".config/sketchybar/plugins/battery.sh".source = mkSketchybarScript "battery" ./plugins/battery.sh;
-      ".config/sketchybar/plugins/clock.sh".source = mkSketchybarScript "clock" ./plugins/clock.sh;
-      ".config/sketchybar/plugins/front_app.sh".source = mkSketchybarScript "front_app" ./plugins/front_app.sh;
-      ".config/sketchybar/plugins/space.sh".source = mkSketchybarScript "space" ./plugins/space.sh;
-      ".config/sketchybar/plugins/volume.sh".source = mkSketchybarScript "volume" ./plugins/volume.sh;
+      file = {
+        ".config/sketchybar/sketchybarrc".source = mkSketchybarScript "sketchybarrc" ./sketchybarrc;
+        ".config/sketchybar/plugins/battery.sh".source = mkSketchybarScript "battery" ./plugins/battery.sh;
+        ".config/sketchybar/plugins/clock.sh".source = mkSketchybarScript "clock" ./plugins/clock.sh;
+        ".config/sketchybar/plugins/front_app.sh".source = mkSketchybarScript "front_app" ./plugins/front_app.sh;
+        ".config/sketchybar/plugins/space.sh".source = mkSketchybarScript "space" ./plugins/space.sh;
+        ".config/sketchybar/plugins/volume.sh".source = mkSketchybarScript "volume" ./plugins/volume.sh;
+      };
     };
-  };
 
-  launchd.agents.sketchybar = {
-    enable = true;
-    config = {
-      Debug = true;
-      Program = "${sketchybarWrapper}";
-      KeepAlive = true;
-      RunAtLoad = true;
-      StandardOutPath = "/tmp/sketchybar.log";
-      StandardErrorPath = "/tmp/sketchybar.err";
-      StartInterval = 5;
+    launchd.agents.sketchybar = {
+      enable = true;
+      config = {
+        Debug = true;
+        Program = "${sketchybarWrapper}";
+        KeepAlive = true;
+        RunAtLoad = true;
+        StandardOutPath = "/tmp/sketchybar.log";
+        StandardErrorPath = "/tmp/sketchybar.err";
+        StartInterval = 5;
+      };
     };
   };
 }
