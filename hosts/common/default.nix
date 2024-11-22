7a5fd4e64615cb00106c1d3bf02d708e5a821eef
Xavier Ruiz <github@xav.ie>
2024-11-22 08:49:16 -0500

    format

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -31,13 +31,13 @@
           "nix-command"
           "flakes"
         ];
-        extra-trusted-public-keys = [
-          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
         extra-trusted-substituters = [
-          "https://devenv.cachix.org"
           "https://nix-community.cachix.org"
+          "https://devenv.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         ];
         fallback = true; # allow building from src
         # use max cores/threads when `enableParallelBuilding` is set for package
