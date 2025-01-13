a73df99819ef9404a15ba4252ce06da829200056
xav-ie <github@xav.ie>
2025-01-13 15:14:23 -0500

    feat(fix-yabai): add
    Sometimes, the yabai daemon gets confused. This fixes that.

---
+++ pkgs/fix-yabai/default.nix
@@ -0,0 +1,31 @@
+{
+  writeShellApplication,
+  yabai,
+  jq,
+  nushell,
+}:
+# TODO: use https://github.com/shanyouli/nur-packages/blob/4365127bfdb0b97919c71d6763d9b9ea2c4d178f/nix/plib/nuenv.nix#L64
+writeShellApplication {
+  name = "fix-yabai";
+  runtimeInputs = [
+    yabai
+    jq
+    nushell
+  ];
+
+  text =
+    let
+      # TODO: format properly
+      nuScript = # nu
+        ''
+          try { sudo yabai --load-sa };
+          yabai -m query --windows | jq '.[].id' | lines | each {|line| try { yabai -m window $line --sub-layer normal }}
+        '';
+    in
+    # sh
+    ''
+      # TODO: remove this, only here to pass shellcheck
+      line=""
+      nu -c "${nuScript}"
+    '';
+}
