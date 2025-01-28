69a173e368fda3041c0fab30d0d5ae0321bbe7d9
Xavier Ruiz <github@xav.ie>
2025-01-28 17:47:36 -0500

    fix(ani-cli): prefer mpv

--- home-manager/programs/mpv/default.nix
+++ home-manager/programs/mpv/default.nix
@@ -4,5 +4,9 @@ _: {
     xdg.mimeApps.defaultApplications = {
       "video/*" = [ "mpv.desktop" ];
     };
+    home.sessionVariables = {
+      # `ani-cli` now prefers to launch `iina`, but I like `mpv`!
+      ANI_CLI_PLAYER = "mpv";
+    };
   };
 }
