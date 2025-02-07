39e9fc7f31cab18d98d1ed3cf87aad7634616933
Xavier Ruiz <github@xav.ie>
2025-02-07 17:09:55 -0500

    fix(nixosConfigurations): use `import`
    just using `inputs.systems` is not sufficient. You must use `import` to
    actually eval the flake input.

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -2,13 +2,12 @@
 # TODO: refactor
 let
   user = "x";
-  hasSystem = system: builtins.elem system (builtins.attrNames inputs.systems);
+  hasSystem = system: builtins.elem system (import inputs.systems);
   addSystem = system: systemConfig: lib.attrsets.optionalAttrs (hasSystem system) systemConfig;
   system = "x86_64-linux";
   # TODO: do this a better way
   configurations =
-    {
-    }
+    { }
     // addSystem system {
       # custom desktop tower
       praesidium = inputs.nixpkgs.lib.nixosSystem {
