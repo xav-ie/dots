e44da829407e3e0a348b85194463dab6ddd904c3
Xavier Ruiz <github@xav.ie>
2025-02-09 22:36:15 -0500

    feat(`waybar`): nixify
    Lots of the `waybar` scripts were just strings. This means `waybar` had
    to be executed in a special environment to work properly. This is
    literally one of the main use cases for `nix`!

--- home-manager/programs/waybar/default.nix
+++ home-manager/programs/waybar/default.nix
@@ -1,14 +1,39 @@
-{ inputs, pkgs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  toplevel,
+  ...
+}:
+let
+  cfg = config.programs.waybar;
+  bars = builtins.attrValues cfg.settings;
+  # go through each bar and get all module names
+  waybar-modules = lib.lists.flatten (
+    builtins.map (
+      barConfig:
+      (barConfig.modules-left or [ ])
+      ++ (barConfig.modules-center or [ ])
+      ++ (barConfig.modules-right or [ ])
+    ) bars
+  );
+
+in
 {
   config = {
+    home.packages =
+      # audio visualizer
+      lib.optional (builtins.elem "cava" waybar-modules) pkgs.cava;
+
     programs.waybar = {
       # https://github.com/elythh/nixdots/blob/58db47f160c219c3e2a9630651dfd9aab0408b1a/modules/home/opt/wayland/services/swaync/default.nix
       enable = true;
       package = inputs.waybar.packages.${pkgs.system}.default;
       settings = {
-        mainBar = import ./config.nix;
+        mainBar = import ./config.nix { inherit lib pkgs toplevel; };
       };
-      style = builtins.readFile (./. + "/style.css");
+      style = builtins.readFile ./style.css;
     };
   };
 }
