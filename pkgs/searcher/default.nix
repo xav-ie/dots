0a3a24d99a372878d20d91c530bf449a2e5f860d
Xavier Ruiz <github@xav.ie>
2025-02-11 08:24:06 -0500

    fix(searcher): misc improvements
    - streaming output to fzf
    - correct parsing of arguments

--- pkgs/searcher/default.nix
+++ pkgs/searcher/default.nix
@@ -1,40 +1,47 @@
 {
-  writeShellApplication,
+  writeNuApplication,
   fzf,
   jq,
+  nix,
+  system,
 }:
-writeShellApplication {
+writeNuApplication {
   name = "searcher";
   runtimeInputs = [
     fzf
     jq
+    nix
+    system
   ];
-  text = ''
-    # Check if two or more arguments are provided
-    if [ $# -ge 2 ]; then
-      # First argument is the repo, the rest are search queries
-      repo="$1"
-      # Shift arguments so "$@" now represents the search queries
-      shift
-    else
-      # Default to "nixpkgs" if less than two arguments are provided
-      repo="nixpkgs"
-    fi
+  text = # nu
+    ''
+      def main [...args: string] {
+        mut args = $args
+        mut repository = "nixpkgs"
+        if ($args | length) > 1 {
+          $repository = $args | first
+          $args = $args | skip 1
+        }
+        let systemName = "x86_64-linux"
+        let prefix = $"* legacyPackages.($systemName)." | str length
+        let ansiCode = "[0;1m" | str length
+        let len = $prefix + $ansiCode + 1 # idk why +1
 
-    # Step 1: Run the nix search and prepare selection options with package names and keys
-    options=$(nix search --json "$repo" "$@" | jq -r 'to_entries [ ] | "\(.value.pname) \(.key)"')
+        let selected = (
+          nix search $repository ...$args 
+          | lines 
+          | filter {|| $in | str starts-with '*'} 
+          | str substring 36..  
+          | str join "\n" 
+          | fzf --ansi --preview="echo {1} | xargs -I {} nix eval --json nixpkgs#{}.meta | jq -C . " -d " "
+        )
 
-    # Step 2: Use fzf to let the user select a package, then generate and display its metadata
-    selected=$(echo "$options" | fzf --ansi --preview-window=right:70%:wrap --preview="echo {2} | xargs -I {} nix eval --json ''${repo}#{}.meta | jq -C . " -d ' ')
-
-    # Extract the package key from the selection for further processing if needed
-    pkg_key=$(echo "$selected" | awk '{print $2}')
-
-    # Step 3: Display metadata of the selected package
-    if [ -n "$pkg_key" ]; then
-      nix shell "nixpkgs#''${pkg_key}"
-    else
-      echo "No package selected."
-    fi
-  '';
+        if ($selected | str length) > 0 {
+          let formatted = $selected | split words | first
+          nix shell $"($repository)#($formatted)"
+        } else {
+          print "No package selected."
+        }
+      }
+    '';
 }
