ad0a05f1bc49cf2165da0ab23819c619c2d8043d
Xavier Ruiz <github@xav.ie>
2025-02-15 19:58:53 -0500

    chore(git): spacing adjustment

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -54,7 +54,8 @@ in
           pr = # sh
             ''
               !(GH_FORCE_TTY=100% gh pr list \
-                | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' --preview-window up --header-lines 3 \
+                | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' \
+                    --preview-window up --header-lines 3 \
                 | awk '{print $1}' \
                 | xargs -r gh pr checkout)
             '';
