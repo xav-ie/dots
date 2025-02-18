d03d29fe697fda7d9644ed6ed685ea20a79baf43
Xavier Ruiz <github@xav.ie>
2025-02-18 17:53:35 -0500

    refactor(mpv): cleanup
    Factor out dependencies into module

--- home-manager/programs/mpv/default.nix
+++ home-manager/programs/mpv/default.nix
@@ -1,6 +1,39 @@
-_: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.programs.mpv;
+in
+{
+  options.programs.mpv = {
+    settings.enable-webtorrent = lib.mkEnableOption "enable-webtorrent";
+  };
   config = {
-    programs.mpv.enable = true;
+    home.packages = lib.optional cfg.settings.enable-webtorrent pkgs.pciutils;
+
+    programs.mpv = {
+      enable = true;
+      package = pkgs.mpv.overrideAttrs (
+        prev: final: {
+          scripts =
+            with pkgs.mpvScripts;
+            [
+              autoload # autoloads entries before and after current entry
+              mpv-playlistmanager # resolves url titles, SHIFT+ENTER for playlist
+              quality-menu # control video quality on the fly
+            ]
+            # extends mpv to handle magnet URLs
+            ++ lib.optional cfg.settings.enable-webtorrent webtorrent-mpv-hook
+            ++
+              # extends mpv to be controllable with MPD
+              lib.optional pkgs.stdenv.isLinux pkgs.mpvScripts.mpris;
+        }
+      );
+      settings.enable-webtorrent = true;
+    };
     xdg.mimeApps.defaultApplications = {
       "video/*" = [ "mpv.desktop" ];
     };
