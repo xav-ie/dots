91d675709db27efab7aa3505d7b50d454d555cc2
Xavier Ruiz <github@xav.ie>
2025-03-07 17:05:17 -0500

    feat(focus-or-open-application): add window switching functionality
    You can now re-activate an application and automatically focus on
    another window instance if it exists.

--- packages/focus-or-open-application/default.nix
+++ packages/focus-or-open-application/default.nix
@@ -1,31 +1,32 @@
 {
   writeNuApplication,
   notify,
+  yabai,
 }:
 writeNuApplication {
   name = "focus-or-open-application";
   runtimeInputs = [
     notify
-    # yabai
+    yabai
   ];
   text = # nu
     ''
       def main [appName: string] {
         try {
-          ^open (mdfind kMDItemContentTypeTree=com.apple.application-bundle
-                | grep $'/($appName).app$')
-          # TODO: add add window switching support (i.e. more than
-          # one window open of app should switch windows on
-          # re-invoke)
-          # Also, make sure PiP windows never get focused on
-          # let appId = (yabai -m query --windows
-          #             | from json
-          #             | where app == $"($appName)"
-          #             | first
-          #             | get id)
-          # yabai -m window --focus $appId
+          # TODO: make sure PiP windows never get focused on
+          let appId = (yabai -m query --windows
+                      | from json
+                      | where app == $"($appName)"
+                      | last
+                      | get id)
+          yabai -m window --focus $appId
         } catch {
-          notify $"Could not focus or open '($appName)'"
+          try {
+            ^open (mdfind kMDItemContentTypeTree=com.apple.application-bundle
+                  | grep $'/($appName).app$')
+          } catch {
+            notify $"Could not focus or open '($appName)'"
+          }
         }
       }
     '';
