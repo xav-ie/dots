7d87d9f3c104e1cbf4054179f9dfaf6ebb02b2b3
Xavier Ruiz <github@xav.ie>
2025-03-10 01:02:53 -0400

    refactor(hass): modularize
    The config has become complex enough.

--- nixosConfigurations/hosts/praesidium/default.nix
+++ nixosConfigurations/hosts/praesidium/default.nix
@@ -15,18 +15,10 @@ in
     inputs.hardware.nixosModules.common-cpu-intel-cpu-only
     inputs.hardware.nixosModules.common-gpu-nvidia-nonprime
     inputs.hardware.nixosModules.common-pc-ssd
-    ./systemd.nix
     ./hardware-configuration.nix
+    ./systemd.nix
+    ./home-assistant.nix
   ];
-
-  options = {
-    services.home-assistant.mediaDir = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      description = "The directory where media is stored";
-      example = "/media";
-      default = null;
-    };
-  };
 
   config = {
     boot = {
@@ -400,74 +392,6 @@ in
       blueman.enable = true;
       flatpak.enable = true;
       geoclue2.enable = true;
-      home-assistant = {
-        enable = true;
-        mediaDir = "/media";
-
-        config = {
-          # You first need to make sure there is at least empty
-          # automations.yaml in /var/lib/hass or this will not work. Same goes
-          # for scripts.yaml, scenes.yaml and groups.yaml
-          # Please see ./systemd.nix
-          automation = "!include automations.yaml";
-          scene = "!include scenes.yaml";
-          script = # yaml
-            "!include scripts.yaml";
-          group = "!include groups.yaml";
-
-          default_config = { };
-
-          homeassistant =
-            let
-              mediaDir = config.services.home-assistant.mediaDir;
-              isDefined = x: x != null;
-            in
-            {
-              temperature_unit = "F";
-              media_dirs = lib.attrsets.optionalAttrs (isDefined mediaDir) { media = mediaDir; };
-              allowlist_external_dirs = lib.lists.optional (isDefined mediaDir) mediaDir;
-            };
-        };
-
-        extraComponents =
-          # default config
-          [
-            "assist_pipeline"
-            "backup"
-            "bluetooth"
-            "cloud"
-            "config"
-            "conversation"
-            "dhcp"
-            "energy"
-            "go2rtc"
-            "history"
-            "homeassistant_alerts"
-            "image_upload"
-            "logbook"
-            "media_source"
-            "mobile_app"
-            "my"
-            "ssdp"
-            "stream"
-            "sun"
-            "usb"
-            "webhook"
-            "zeroconf"
-          ]
-          # my additions
-          ++ [
-            "apple_tv"
-            "govee_light_local"
-            "homekit"
-            "homekit_controller"
-            "matter"
-            "met"
-            "thread"
-            "xiaomi_aqara"
-            "zha"
-          ];
-      };
 
       gnome.gnome-keyring.enable = true;
       openssh = {
