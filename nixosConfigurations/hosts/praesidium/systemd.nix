8c899227d9fbade5944a743c9216ebb352e4da6e
Xavier Ruiz <github@xav.ie>
2025-03-09 21:52:51 -0400

    fix(hass): create typical default files
    For whatever reason, the default config does not set up these files even
    though they are required for these home-assistant features to function
    properly.

---
+++ nixosConfigurations/hosts/praesidium/systemd.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  config = {
+    systemd = {
+      user.services.polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wantedBy = [ "default.target" ];
+        after = [ "default.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
+
+      # fix bug in ha not creating these files...
+      tmpfiles.rules =
+        let
+          hassDir = config.services.home-assistant.configDir;
+        in
+        lib.lists.optionals config.services.home-assistant.enable [
+          "f ${hassDir}/automations.yaml 0755 hass hass"
+          "f ${hassDir}/scenes.yaml      0755 hass hass"
+          "f ${hassDir}/scripts.yaml     0755 hass hass"
+          # "d /var/lib/hass/backups 0750 hass hass"
+        ];
+
+      # must be system service due to journalctl needing elevated permissions
+      # TODO: ^ is this true?
+      services.clear-log = {
+        description = "Clear >1 month-old logs every week";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.systemd}/bin/journalctl --vacuum-time=21d";
+        };
+      };
+      timers.clear-log = {
+        wantedBy = [ "timers.target" ];
+        partOf = [ "clear-log.service" ];
+        timerConfig.OnCalendar = "weekly UTC";
+      };
+    };
+  };
+}
