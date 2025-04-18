7a94482d6faa485e146cddef2eb11909dfb9e17e
Xavier Ruiz <github@xav.ie>
2025-04-18 17:45:05 -0400

    fix(`notify`): explicitly depend on `generate-kaomoji`
    Not depending can cause it to break from cleaning the store.

--- packages/notify/default.nix
+++ packages/notify/default.nix
@@ -1,19 +1,21 @@
 {
-  lib,
   writeNuApplication,
   libnotify,
   generate-kaomoji,
 }:
 writeNuApplication {
   name = "notify";
-  runtimeInputs = [ libnotify ];
+  runtimeInputs = [
+    libnotify
+    generate-kaomoji
+  ];
   text = # nu
     ''
       # Send a notification on Mac or Linux. If no body is provided, it
       # generates a random kaomoji for you ☆⌒(ゝ。∂)
       def main [title: string, body?: string] {
         let body = match $body {
-          null => (${lib.getExe generate-kaomoji} -r ".value")
+          null => (generate-kaomoji -r ".value")
           _ => $body
         }
         match (uname | get kernel-name) {
