9283b23b9dd2b64a813bb7f678c9167c4bb90de9
Xavier Ruiz <github@xav.ie>
2025-04-30 01:16:57 -0400

    feat(`git review`): add
    This command makes it miles easier to review PRs. Combine this with `git
    pr`, and reviewing PRs will be very quick. `git review` just resets the
    git repo to the base branch of the PR. This means all the changed files
    become working, making in-editor review extremely simple and easy.

---
+++ packages/review/default.nix
@@ -0,0 +1,60 @@
+{
+  writeNuApplication,
+  git,
+  gh,
+}:
+writeNuApplication {
+  name = "review";
+  runtimeInputs = [
+    git
+    gh
+  ];
+  text = # nu
+    ''
+      def spinner [] {
+        ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+        | each { |x|
+          print -n $"(ansi green)($x)(ansi reset) Fetching remote data...\r"
+          sleep 35ms
+        }
+        return
+      }
+
+      # Use this command to reset the git repo to the base branch of the PR.
+      # Useful for reviewing changed files in-editor.
+      def main [] {
+        # 1. Check we are clean and good to reset
+        let git_status = (git status --porcelain)
+        if ($git_status | str trim | str length) > 0 {
+          error make --unspanned { msg: "Error: Git working directory is not clean." }
+        }
+
+        # 2. Get the baseRefName
+        let temp_out = (mktemp)
+        let fetch_job_id = job spawn {
+          try {
+            git pull;
+            gh pr view --json baseRefName
+            | from json
+            | get baseRefName
+            | save -f $temp_out
+          }
+        }
+        while (job list | where id == $fetch_job_id | length) == 1 {
+          spinner
+        }
+
+        let $baseRefName = (open $temp_out)
+        rm $temp_out
+        if ($baseRefName == "") {
+          error make --unspanned { msg: "Error: No pull request found." }
+        }
+
+        # 3. Reset to base branch but keep all changes as uncommitted
+        git reset $baseRefName
+
+        # 4. Intend to add all files so they show up in (n)vim
+        git add -N .
+      }
+    '';
+}
