8cd9a17fd0eef415e06503123da782f259d836d5
Xavier Ruiz <github@xav.ie>
2025-05-01 17:27:52 -0400

    refactor(packages): import .nu files
    This allows for simpler editing, better github diff, and puts the files
    properly into github indexing.

--- packages/fix-yabai/default.nix
+++ packages/fix-yabai/default.nix
@@ -2,27 +2,13 @@
   writeNuApplication,
   yabai,
   jq,
-  nushell,
 }:
-# TODO: use https://github.com/shanyouli/nur-packages/blob/4365127bfdb0b97919c71d6763d9b9ea2c4d178f/nix/plib/nuenv.nix#L64
-
 writeNuApplication {
   name = "fix-yabai";
   runtimeInputs = [
     yabai
     jq
-    nushell
   ];
 
-  text = # nu
-    ''
-      try { sudo yabai --load-sa };
-
-      yabai -m query --windows
-      | jq '.[].id'
-      | lines
-      | each { |line|
-          try { yabai -m window $line --sub-layer normal }
-        }
-    '';
+  text = builtins.readFile ./fix-yabai.nu;
 }
