cf9e786fa41256f2535641e619f867396e5e8eaf
Xavier Ruiz <github@xav.ie>
2025-05-13 01:51:48 -0400

    feat(spdf): set up with podman
    This is mainly an exercise to show that I can configure podman
    containers through Nix. Striling PDF is also pretty cool.

---
+++ nixosConfigurations/hosts/praesidium/virtualisation/default.nix
@@ -0,0 +1,24 @@
+{ ... }:
+{
+  imports = [
+    ./spdf.nix
+  ];
+
+  config = {
+    virtualisation = {
+      podman = {
+        enable = true;
+        # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerCompat = true;
+        # Create a "docker" socket that just points to podman
+        dockerSocket.enable = true;
+        # Required for containers under podman-compose to be able to talk to each other.
+        defaultNetwork.settings.dns_enabled = true;
+      };
+    };
+    # generally good to have this set up
+    systemd.tmpfiles.rules = [
+      "d /media 0777 root root -"
+    ];
+  };
+}
