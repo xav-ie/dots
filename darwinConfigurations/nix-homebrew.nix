1444a12ff84b3bea3fa307f862c292e051b93ea4
Xavier Ruiz <github@xav.ie>
2025-05-29 17:02:29 -0400

    feat(tailscale): enable on macos
    The tailscale app is pretty much unbeatable, so just use it.
    
    I also added auto-setup of the `tailscale` cli tool if the mas app is
    defined.

--- darwinConfigurations/nix-homebrew.nix
+++ darwinConfigurations/nix-homebrew.nix
@@ -1,6 +1,8 @@
 {
-  inputs,
   config,
+  inputs,
+  lib,
+  pkgs,
   ...
 }:
 {
@@ -8,25 +10,37 @@
     inputs.nix-homebrew.darwinModules.nix-homebrew
   ];
 
-  # https://github.com/zhaofengli/nix-homebrew/issues/5
-  # You must tell nix-darwin to just inherit the same taps as nix-homebrew
-  homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+  config = {
+    # https://github.com/zhaofengli/nix-homebrew/issues/5
+    # You must tell nix-darwin to just inherit the same taps as nix-homebrew
+    homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
 
-  nix-homebrew = {
-    # Install Homebrew under the default prefix
-    enable = true;
-    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-    enableRosetta = true;
-    # User owning the Homebrew prefix
-    user = config.defaultUser;
-    # Optional: Declarative tap management
-    taps = {
-      "homebrew/homebrew-core" = inputs.homebrew-core;
-      "homebrew/homebrew-cask" = inputs.homebrew-cask;
-      "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
+    nix-homebrew = {
+      # Install Homebrew under the default prefix
+      enable = true;
+      # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+      enableRosetta = true;
+      # User owning the Homebrew prefix
+      user = config.defaultUser;
+      # Optional: Declarative tap management
+      taps = {
+        "homebrew/homebrew-core" = inputs.homebrew-core;
+        "homebrew/homebrew-cask" = inputs.homebrew-cask;
+        "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
+      };
+      # Optional: Enable fully-declarative tap management
+      # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+      mutableTaps = false;
     };
-    # Optional: Enable fully-declarative tap management
-    # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-    mutableTaps = false;
+
+    environment.systemPackages = lib.optionals (config.homebrew.masApps ? "Tailscale") [
+      (pkgs.writeShellApplication {
+        name = "tailscale";
+        meta.description = "create a symlink to the tailscale binary provided by MacOS app";
+        text = ''
+          /Applications/Tailscale.app/Contents/MacOS/Tailscale "$@"
+        '';
+      })
+    ];
   };
 }
