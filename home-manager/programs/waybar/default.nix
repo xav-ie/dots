c189c9123391547dbff3bd8cd24b1201297bbdfa
Xavier Ruiz <github@xav.ie>
2025-06-19 08:16:38 -0400

    feat(hyprland): move startup programs into services
    This should guarantee greater reliability and flexibility to switch to
    other desktop systems if desired.

--- home-manager/programs/waybar/default.nix
+++ home-manager/programs/waybar/default.nix
@@ -39,6 +39,7 @@ in
     programs.waybar = {
       # https://github.com/elythh/nixdots/blob/58db47f160c219c3e2a9630651dfd9aab0408b1a/modules/home/opt/wayland/services/swaync/default.nix
       enable = true;
+      systemd.enable = true;
       package = inputs.waybar.packages.${pkgs.system}.default;
       settings = {
         mainBar = import ./config.nix {
