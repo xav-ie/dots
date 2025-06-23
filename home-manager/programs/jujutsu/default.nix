06c8c3e662da3c9fb09146dfa2b8e6ccea96c82f
Xavier Ruiz <github@xav.ie>
2025-06-23 08:08:45 -0400

    feat(jj): add

---
+++ home-manager/programs/jujutsu/default.nix
@@ -0,0 +1,51 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  config = {
+    programs.jujutsu = {
+      enable = true;
+      package =
+        inputs.jj.outputs.packages.${pkgs.stdenv.hostPlatform.system}.jujutsu.overrideAttrs
+          (old: {
+            sandboxProfile = "";
+            # __sandboxProfile = null;
+            doCheck = false;
+          });
+      settings = {
+        "$schema" = "https://jj-vcs.github.io/jj/latest/config-schema.json";
+        # https://jj-vcs.github.io/jj/latest/config/#commit-signing
+        signing = {
+          behavior = "own";
+          backend = "gpg";
+          # github@xav.ie
+          key = "5B9134A9E7E7F965";
+        };
+        ui = {
+          show-cryptographic-signatures = true;
+        };
+        user = {
+          name = "Xavier Ruiz";
+          email = "github@xav.ie";
+        };
+
+        "--scope" = [
+          {
+            # NB: only applies properly when there is active jj repo
+            "--when"."repositories" = [ "~/Work/" ];
+            user.email = "xavier@outsmartly.com";
+            # xavier@outsmartly.com
+            signing.key = "22420DD6C13E3EB7";
+          }
+        ];
+
+        # TODO:
+        # - pager
+        # - diff editor
+        # - rebase stuff
+      };
+    };
+  };
+}
