62acc76f0e677e029dbebe256caf91a1e963eeb7
Xavier Ruiz <github@xav.ie>
2025-07-08 17:31:35 -0400

    fix(git remotes): do not set
    In general, setting a git remote config causes issues :(.
    This is because remote conflicts of multiple refs.

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -213,14 +213,14 @@ in
         # https://andrewlock.net/working-with-stacked-branches-in-git-is-easier-with-update-refs/
         # to temporarily turn off, --no-update-refs
         rebase.updateRefs = true;
-        remote.origin.fetch = [
-          # Normal branches - set up by default
-          "+refs/heads/*:refs/remotes/origin/*"
-          # PR head commits
-          "+refs/pull/*/head:refs/remotes/origin/pull/*"
-          # Merge PR commits, disabled because noisy, but might be useful in future
-          # "+refs/pull/*/head:refs/remotes/origin/pr/*"
-        ];
+        # remote.origin.fetch = [
+        #   # Normal branches - set up by default
+        #   "+refs/heads/*:refs/remotes/origin/*"
+        #   # PR head commits
+        #   "+refs/pull/*/head:refs/remotes/origin/pull/*"
+        #   # Merge PR commits, disabled because noisy, but might be useful in future
+        #   # "+refs/pull/*/head:refs/remotes/origin/pr/*"
+        # ];
         rerere.enabled = true;
       };
 
