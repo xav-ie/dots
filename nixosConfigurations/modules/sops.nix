648542b7615c48abf454b40c8fd58a6118ef5d3f
Xavier Ruiz <github@xav.ie>
2025-10-06 19:46:53 -0400

    fix(sops git/allowed_signers): move into shared config
    nix-darwin does not have access to the module.

--- nixosConfigurations/modules/sops.nix
+++ nixosConfigurations/modules/sops.nix
@@ -1,14 +1,7 @@
-{ config, inputs, ... }:
+{ inputs, ... }:
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
     ../../lib/common/sops.nix
   ];
-
-  config = {
-    sops.secrets."git/allowed_signers" = {
-      owner = config.defaultUser;
-      mode = "0444";
-    };
-  };
 }
