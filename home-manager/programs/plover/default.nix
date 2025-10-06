1a531d2f7cf41afaa5bd2bf44decb7bee5af6afc
Xavier Ruiz <github@xav.ie>
2025-10-06 19:36:15 -0400

    docs(plover): add mnemonic

--- home-manager/programs/plover/default.nix
+++ home-manager/programs/plover/default.nix
@@ -2,6 +2,10 @@
 {
   # https://docs.stenokeyboards.com/
   # https://github.com/openstenoproject/plover-flake
+  # STKPWHRAO*EUFRPBLGTSDZ
+  # Seven   Tigers Prowling Hunters *          *     Flew     Past   Leaving Through Dark
+  # (silly) Kept   While    Ran     *          *     Rapidly  Beasts Glitter Strange Zones.
+  #                #        Around  Outside.   Eight Unicorns #
   imports = [
     inputs.plover-flake.homeManagerModules.plover
   ];
