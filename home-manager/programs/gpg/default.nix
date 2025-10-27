6a5c3f5c952d254e3c2939cb206583b9552b360b
Xavier Ruiz <github@xav.ie>
2025-10-27 03:04:02 -0400

    refactor(gpg): cleanup config

--- home-manager/programs/gpg/default.nix
+++ home-manager/programs/gpg/default.nix
@@ -1,5 +1,22 @@
-_: {
+{ lib, pkgs, ... }:
+let
+  inherit (pkgs.stdenv) isLinux;
+in
+{
   programs.gpg = {
     enable = true;
   };
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  }
+  // lib.optionalAttrs isLinux {
+    pinentry.package = pkgs.pinentry-gnome3;
+  };
+
+  # Add pinentry-gnome3 to packages on Linux
+  home.packages = lib.optionals isLinux [
+    pkgs.pinentry-gnome3
+  ];
 }
