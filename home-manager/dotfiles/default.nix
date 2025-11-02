5323d7761841df7dc03e659d9408e3ea686c09f2
Xavier Ruiz <github@xav.ie>
2025-11-02 17:00:21 -0500

    refactor(ghostty): move config into shared programs area for simplificity

--- home-manager/dotfiles/default.nix
+++ home-manager/dotfiles/default.nix
@@ -1,5 +1,9 @@
 _: {
-  imports = [
-    ./ghostty
-  ];
+  config = {
+    # There has got to be a better way to do this :(
+    home.file.".config/scripts/remove_video_silence.py".source = ./remove_video_silence.py;
+    home.file.".config/gh-dash/config.yml".source = ./gh-dash/config.yml;
+    home.file.".config/uair/uair.toml".source = ./uair.toml;
+    home.file.".config/pijul/config.toml".source = ./pijul/config.toml;
+  };
 }
