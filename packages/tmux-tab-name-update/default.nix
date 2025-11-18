1f7bff7142514ab3634d377c233928f938f6dba9
Xavier Ruiz <github@xav.ie>
2025-11-18 12:06:04 -0500

    fix(darwin): misc packages

--- packages/tmux-tab-name-update/default.nix
+++ packages/tmux-tab-name-update/default.nix
@@ -7,11 +7,20 @@ pkgsStatic.stdenv.mkDerivation {
 
   dontUnpack = true;
 
-  buildPhase = ''
-    $CC -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables \
-      -fno-stack-protector -ffunction-sections -fdata-sections \
-      -Wl,--gc-sections -static -s -o tmux-tab-name-update $src
-  '';
+  buildPhase =
+    if pkgsStatic.stdenv.isDarwin then
+      # macOS doesn't support fully static linking
+      ''
+        $CC -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables \
+          -fno-stack-protector -dead_strip -o tmux-tab-name-update $src
+      ''
+    else
+      # Linux static build
+      ''
+        $CC -Os -fomit-frame-pointer -fno-asynchronous-unwind-tables \
+          -fno-stack-protector -ffunction-sections -fdata-sections \
+          -Wl,--gc-sections -static -s -o tmux-tab-name-update $src
+      '';
 
   installPhase = ''
     mkdir -p $out/bin
