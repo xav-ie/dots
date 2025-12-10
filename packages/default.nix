2a92d07d0f431088ebed01dbc91f75033a768e9e
Xavier Ruiz <github@xav.ie>
2025-12-09 23:20:55 -0500

    feat(nom/nom-run): set up in devShell

--- packages/default.nix
+++ packages/default.nix
@@ -9,14 +9,12 @@ let
   optionalAttrs = bool: attrSet: if bool then attrSet else { };
   writeNuApplication = nuenv.mkNushellScriptApplication pkgs.nushell pkgs.writeTextFile pkgs.lib;
   notify = pkgs.callPackage ./notify { inherit generate-kaomoji writeNuApplication; };
-  base-ref = pkgs.callPackage ./base-ref { inherit writeNuApplication; };
-  zenity-askpass = pkgs.callPackage ./zenity-askpass { inherit writeNuApplication; };
-  prettier-plugin-toml = pkgs.callPackage ./prettier-plugin-toml { };
 in
 rec {
-  inherit base-ref notify prettier-plugin-toml;
+  inherit notify;
   default = pkgs.callPackage ./cache-command { };
   apple-emoji-linux = pkgs.callPackage ./apple-emoji-linux { };
+  base-ref = pkgs.callPackage ./base-ref { inherit writeNuApplication; };
   better-branch = pkgs.callPackage ./better-branch { inherit writeNuApplication; };
   cache-command = pkgs.callPackage ./cache-command { };
   claude-code = pkgs.callPackage ./claude-code { };
@@ -33,9 +31,10 @@ rec {
   log-pr = pkgs.callPackage ./log-pr { inherit writeNuApplication; };
   nix-output-monitor = pkgs.callPackage ./nix-output-monitor { };
   nix-repl = pkgs.callPackage ./nix-repl { inherit writeNuApplication; };
-  nom-run = pkgs.callPackage ./nom-run { inherit writeNuApplication; };
+  nom-run = pkgs.callPackage ./nom-run { inherit nix-output-monitor writeNuApplication; };
   nvim = pkgs.callPackage ./nvim { };
   pgpod = pkgs.callPackage ./pgpod { inherit writeNuApplication; };
+  prettier-plugin-toml = pkgs.callPackage ./prettier-plugin-toml { };
   prettier-with-toml = pkgs.callPackage ./prettier-with-toml { inherit prettier-plugin-toml; };
   prs = pkgs.callPackage ./prs { inherit writeNuApplication; };
   pr-summary = pkgs.callPackage ./pr-summary { inherit base-ref writeNuApplication; };
@@ -59,11 +58,11 @@ rec {
   move-pip = pkgs.callPackage ./move-pip { inherit writeNuApplication; };
 })
 // (optionalAttrs pkgs.stdenv.isLinux {
-  inherit zenity-askpass;
   move-active = pkgs.callPackage ./move-active { inherit writeNuApplication; };
   openrgb-appimage = pkgs.callPackage ./openrgb-appimage { };
   record = pkgs.callPackage ./record { };
   record-section = pkgs.callPackage ./record-section { };
   rofi-cliphist = pkgs.callPackage ./rofi-cliphist { inherit writeNuApplication; };
   rofi-powermenu = pkgs.callPackage ./rofi-powermenu { inherit writeNuApplication; };
+  zenity-askpass = pkgs.callPackage ./zenity-askpass { inherit writeNuApplication; };
 })
