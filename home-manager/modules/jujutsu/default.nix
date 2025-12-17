8e4976a80183eec061d9f7f79734bc9029b8f8e7
Xavier Ruiz <github@xav.ie>
2025-12-17 15:11:32 -0500

    chore(delta-jj): factor out

--- home-manager/modules/jujutsu/default.nix
+++ home-manager/modules/jujutsu/default.nix
@@ -35,23 +35,7 @@
             pkgs.writeNuApplication {
               name = "delta-jj";
               runtimeInputs = [ pkgs.delta ];
-              text = # nu
-                ''
-                  def --wrapped main [...args] {
-                    # #200030
-                    let section_bg = $"(ansi -e '48;2;32;0;48m')"
-                    delta --width (term size | get columns) ...$args
-                    | lines
-                    | par-each -k {|line|
-                      if ("Δ" in ($line | ansi strip)) {
-                        $"($section_bg)($line)(ansi reset)"
-                      } else {
-                        $line
-                      }
-                    }
-                    | str join "\n"
-                  }
-                '';
+              text = builtins.readFile ./delta-jj.nu;
             }
           );
         };
