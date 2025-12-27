b1827e016965cc5897aa5f9fe2fe87ace34d02bd
Xavier Ruiz <github@xav.ie>
2025-12-27 18:16:52 -0500

    fix(waybar): use all my upstreamed patches
    There is no frontend/backend config that breaks a lot of things.

--- home-manager/linux/waybar/config.nix
+++ home-manager/linux/waybar/config.nix
@@ -107,11 +107,11 @@ in
   cava = {
     framerate = 60;
     autosens = 0;
-    sensitivity = 5;
+    sensitivity = 2;
     bars = 12;
     lower_cutoff_freq = 50;
     higher_cutoff_freq = 10000;
-    method = "pulse";
+    method = "pipewire";
     source = "auto";
     stereo = false;
     reverse = false;
