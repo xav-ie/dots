2c788b9accbf7c09e939ade5f5c2ea323fca9e2f
Xavier Ruiz <github@xav.ie>
2026-01-30 12:00:34 -0500

    fix(bluetooth headsets): do not switch automatically
    This causes wireplumber to crash for some reason and also unwanted
    anyways.

--- nixosConfigurations/hosts/praesidium/default.nix
+++ nixosConfigurations/hosts/praesidium/default.nix
@@ -433,6 +433,15 @@ in
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
+        # Disable Bluetooth autoswitch due to WirePlumber 0.5.13 bug
+        # https://gitlab.freedesktop.org/pipewire/wireplumber/-/issues/682
+        # This prevents crashes when AirPods connect. Manual profile switching
+        # may be needed for microphone use.
+        wireplumber.extraConfig."50-disable-bluetooth-autoswitch" = {
+          "wireplumber.settings" = {
+            "bluetooth.autoswitch-to-headset-profile" = false;
+          };
+        };
       };
       resolved = {
         enable = false;
