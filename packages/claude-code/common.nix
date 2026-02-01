916c0f8add2ae9240028dca4a87ae0f642ebe993
Xavier Ruiz <github@xav.ie>
2026-01-31 20:32:38 -0500

    feat(linuxBinPath): optimize eval

--- packages/claude-code/common.nix
+++ packages/claude-code/common.nix
@@ -8,6 +8,13 @@ let
     gopls
     rust-analyzer
     ;
+
+  linuxBinPath = lib.makeBinPath [
+    socat
+    bubblewrap
+    gopls
+    rust-analyzer
+  ];
 in
 {
   # Dependencies needed on Linux for sandboxing
@@ -17,22 +24,16 @@ in
   ];
 
   # Common wrapper arguments for wrapProgram
-  wrapperArgs = lib.concatStringsSep " " [
-    "--set DISABLE_AUTOUPDATER 1"
-    "--set ENABLE_TOOL_SEARCH true"
-    "--set ENABLE_LSP_TOOL true"
-    "--set ENABLE_EXPERIMENTAL_MCP_CLI false"
-    "--set CLAUDE_CODE_ADDITIONAL_DIRECTORIES_CLAUDE_MD 1"
-    (lib.optionalString stdenv.isLinux "--prefix PATH : ${
-      lib.makeBinPath [
-        socat
-        bubblewrap
-        # lsp server support
-        gopls
-        rust-analyzer
-      ]
-    }")
-  ];
+  wrapperArgs = lib.concatStringsSep " " (
+    [
+      "--set DISABLE_AUTOUPDATER 1"
+      "--set ENABLE_TOOL_SEARCH true"
+      "--set ENABLE_LSP_TOOL true"
+      "--set ENABLE_EXPERIMENTAL_MCP_CLI false"
+      "--set CLAUDE_CODE_ADDITIONAL_DIRECTORIES_CLAUDE_MD 1"
+    ]
+    ++ lib.optional stdenv.isLinux "--prefix PATH : ${linuxBinPath}"
+  );
 
   # Shared meta attributes
   meta =
