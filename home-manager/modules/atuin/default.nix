0c53f80010d82acf30eeb902befe941b5336eb02
Xavier Ruiz <github@xav.ie>
2026-03-22 16:40:44 -0400

    chore(atuin): update

--- home-manager/modules/atuin/default.nix
+++ home-manager/modules/atuin/default.nix
@@ -3,11 +3,9 @@
   config = {
     programs.atuin = {
       enable = true;
-      package = pkgs.pkgs-bleeding.atuin;
       # super buggy on macos
       daemon.enable = pkgs.stdenv.isLinux;
       enableZshIntegration = false;
-      enableNushellIntegration = false;
       # https://docs.atuin.sh/configuration/config
       settings = {
         style = "compact";
